3462ee91841db017f0234924b4730d06eb9f032d
Aya abuyousef <62219770+Ayaabuyousef@users.noreply.github.com>
2020-08-23 10:24:02 +0300

    Update W2D2.js
    W2D2 toy problem solved

--- W2D2.js
+++ W2D2.js
@@ -1,10 +1,17 @@
 // 1-using the console calculate the average age of the follwing ages [13,14,13,15,16,17,19,13,16,15].
+console.log([13+14+13+15+16+17+19+13+16+15]/10)
+
 // 2-using the console calculate your age in seconds.
+(25*365*24*60)
+
 
 // 3- Write a function identity that takes one parameter and returns that input value.
 // Calling your function should result in:
 // identity("hello world");  ==> "hello world"
 // identity(500);  ==> 500
+function identity (x){
+  return x
+}
 
 // 4- Write a function convertTo that takes  a string and a number as parameters.
 // If the string input is "cm", then the function should convert the 2nd argument into centimeters by multiplying it to 2.54 and returning that value.
@@ -14,7 +21,11 @@
 // convertTo('in', 50.8); ==> 20
 
 function convertTo(units, num) {
-  // write your code here
+  if(units==="cm")
+    return num*2.54
+}
+else if (string==="in"){
+  return num/2.54
 }
 
 //4- Write a function dogsIWouldPet that takes an item (string), 
@@ -25,10 +36,16 @@ function convertTo(units, num) {
 // dogsIWouldPet("Terrier"); //"I would pet dogs no bigger than an Terrier"
 // Bonus (extra): If your function were passed in a string "I do not like dogs", 
 //you can have your function return "I would not pet dogs".
-
+function dogsIwouldpet(string){
+  if(string==="i do not like dogs")
+    return "i would not pet dogs"
+}
 
 // 5- Write a function convertToKilometers that takes a number of miles passed in as parameter, 
 // and returns that number multiplied by 1.60934 (an accepted approximation of 1 mile in kilometers).
 // Calling your function should result in:
 // convertToKilometers(50);   ==> 80.467
 // convertToKilometers(361);  ==> 580.973
+function convertTokilometers(n){
+  return n*1.60934
+}
