b2773f4c52dba93977ee03ce21e50e3fd8af3f20
RamiOkkeh <ramiokkeh@gmail.com>
2020-08-23 10:16:44 +0300

    my answers

--- W2D2.js
+++ W2D2.js
@@ -1,10 +1,27 @@
 // 1-using the console calculate the average age of the follwing ages [13,14,13,15,16,17,19,13,16,15].
+function average(arr){
+	var sum = 0
+	for (var i = 0; i < arr.length; i++) {
+		sum += arr[i]
+	} return sum / i
+}
+console.log(average([13,14,13,15,16,17,19,13,16,15])) // result = 15.1
+
 // 2-using the console calculate your age in seconds.
+function yearsToSeconds(age) {
+	return age * 525600 * 60
+}
+console.log(yearsToSeconds(21)) // result = 662256000
 
 // 3- Write a function identity that takes one parameter and returns that input value.
 // Calling your function should result in:
 // identity("hello world");  ==> "hello world"
 // identity(500);  ==> 500
+function identity(param) {
+	return param
+}
+console.log(identity("hello world"))
+console.log(identity(500))
 
 // 4- Write a function convertTo that takes  a string and a number as parameters.
 // If the string input is "cm", then the function should convert the 2nd argument into centimeters by multiplying it to 2.54 and returning that value.
@@ -14,8 +31,15 @@
 // convertTo('in', 50.8); ==> 20
 
 function convertTo(units, num) {
-  // write your code here
+  if (units === 'cm') {
+  	num *= 2.54
+  } else {
+  	num /= 2.54
+  }
+  return num
 }
+console.log(convertTo('cm', 100)) // result = 254
+console.log(convertTo('in', 50.8)) // result = 20
 
 //4- Write a function dogsIWouldPet that takes an item (string), 
 //and returns a sentence stating dogs you would pet in comparison to the item (see sample call below).
@@ -26,9 +50,23 @@ function convertTo(units, num) {
 // Bonus (extra): If your function were passed in a string "I do not like dogs", 
 //you can have your function return "I would not pet dogs".
 
+function dogsIWouldPet(input) {
+	if (input === 'I do not like dogs') {
+		return 'I would not pet dogs'
+	}
+	return 'I would pet dogs no bigger than an ' + input
+}
+console.log(dogsIWouldPet('ottoman'))
 
 // 5- Write a function convertToKilometers that takes a number of miles passed in as parameter, 
 // and returns that number multiplied by 1.60934 (an accepted approximation of 1 mile in kilometers).
 // Calling your function should result in:
 // convertToKilometers(50);   ==> 80.467
 // convertToKilometers(361);  ==> 580.973
+function convertToKilometers(miles) {
+	return miles * 1.60934
+}
+console.log(convertToKilometers(50)) // result = 80.467
+console.log(convertToKilometers(361)) // result = 580.97174
+
+
