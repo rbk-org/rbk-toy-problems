34abfd86e84a3275aedd888da102496ce84a8ab1
hamdalla2 <hamdalla2@yahoo.com>
2020-08-23 10:34:53 +0300

    solved

--- W2D2.js
+++ W2D2.js
@@ -1,10 +1,24 @@
 // 1-using the console calculate the average age of the follwing ages [13,14,13,15,16,17,19,13,16,15].
+
+function ageAverage(){
+	s=13+14+13+15+16+17+19+13+16+15
+		return s/10
+}
+
 // 2-using the console calculate your age in seconds.
+
+function ageSeconds(num){
+	return num*365*24*60*60
+}
 
 // 3- Write a function identity that takes one parameter and returns that input value.
 // Calling your function should result in:
 // identity("hello world");  ==> "hello world"
 // identity(500);  ==> 500
+
+function identity (x){
+	return x
+}
 
 // 4- Write a function convertTo that takes  a string and a number as parameters.
 // If the string input is "cm", then the function should convert the 2nd argument into centimeters by multiplying it to 2.54 and returning that value.
@@ -14,10 +28,11 @@
 // convertTo('in', 50.8); ==> 20
 
 function convertTo(units, num) {
-  // write your code here
+	if(unit==="cm"){return num*2.54}
+	if(unit==="in"){return num/2.54}
 }
 
-//4- Write a function dogsIWouldPet that takes an item (string), 
+//5- Write a function dogsIWouldPet that takes an item (string), 
 //and returns a sentence stating dogs you would pet in comparison to the item (see sample call below).
 // Calling your function should result in:
 // dogsIWouldPet("ottoman"); //"I would pet dogs no bigger than an ottoman"
@@ -26,9 +41,18 @@ function convertTo(units, num) {
 // Bonus (extra): If your function were passed in a string "I do not like dogs", 
 //you can have your function return "I would not pet dogs".
 
+function dogsIWouldPet(string){
+	if(typeOf(string)==="string"){
+	return "I would pet dogs no bigger than an "+string}
+	return "I do not like dogs"
+}
 
-// 5- Write a function convertToKilometers that takes a number of miles passed in as parameter, 
+// 6- Write a function convertToKilometers that takes a number of miles passed in as parameter, 
 // and returns that number multiplied by 1.60934 (an accepted approximation of 1 mile in kilometers).
 // Calling your function should result in:
 // convertToKilometers(50);   ==> 80.467
 // convertToKilometers(361);  ==> 580.973
+
+function convertToKilometers(miles){
+return miles*1.60934
+}
