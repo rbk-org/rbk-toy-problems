c2ecdb7249c2c13d912690a33343a06a803b86c8
GitHub-MahmoudWaked <mahmoud_waked@hotmail.com>
2020-08-23 10:16:28 +0300

    my answer

--- W2D2.js
+++ W2D2.js
@@ -1,10 +1,28 @@
 // 1-using the console calculate the average age of the follwing ages [13,14,13,15,16,17,19,13,16,15].
+function average(){
+var arr1 = [13,14,13,15,16,17,19,13,16,15]
+var sum = 0
+for (var i = 0 ; i < arr1.length ; i++){
+sum += arr1[i]
+}
+return sum / (i)
+}
+console.log(average()) // result = 15.1
 // 2-using the console calculate your age in seconds.
-
+function yearsToseconds (age){
+return age * 525600 * 60
+}
+console.log (yearsToseconds(29)) // result = 914544000
 // 3- Write a function identity that takes one parameter and returns that input value.
 // Calling your function should result in:
 // identity("hello world");  ==> "hello world"
 // identity(500);  ==> 500
+function identity(param){
+	return param
+}
+console.log(identity("hello world")) // result = hello world
+console.log(identity(500)) // result = 500
+
 
 // 4- Write a function convertTo that takes  a string and a number as parameters.
 // If the string input is "cm", then the function should convert the 2nd argument into centimeters by multiplying it to 2.54 and returning that value.
@@ -14,10 +32,25 @@
 // convertTo('in', 50.8); ==> 20
 
 function convertTo(units, num) {
-  // write your code here
-}
 
-//4- Write a function dogsIWouldPet that takes an item (string), 
+  if (units === "cm"){
+  	num *= 2.54
+  }else if (units === "in"){
+  	num /= 2.54
+  }
+  return num
+}
+console.log (convertTo('cm', 100)) // result = 254
+console.log (convertTo('in', 50.8)) // result = 20
+//4- Write a function dogsIWouldPet that tak{es an item (string),
+function dogsIWouldPet(input){
+	if (input === "I don't like dogs"){
+		return "I would not pet dogs"
+	}
+	return "I would pet dogs no bigger than an " + input
+}
+console.log(dogsIWouldPet('ottoman'))
+
 //and returns a sentence stating dogs you would pet in comparison to the item (see sample call below).
 // Calling your function should result in:
 // dogsIWouldPet("ottoman"); //"I would pet dogs no bigger than an ottoman"
@@ -32,3 +65,8 @@ function convertTo(units, num) {
 // Calling your function should result in:
 // convertToKilometers(50);   ==> 80.467
 // convertToKilometers(361);  ==> 580.973
+function convertToKilometers(miles){
+	return miles * 1.60934
+}
+console.log(convertToKilometers(50)) // result = 80.467
+console.log(convertToKilometers(361)) // result = 580.97174
