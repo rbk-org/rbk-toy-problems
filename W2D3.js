d09c138b9feb6f22dea3a1476cd53d9862f8541f
Yasir-Tbaileh <yasur96@gmail.com>
2020-08-24 12:50:08 +0300

    W2D3 solved bu Yasir

--- W2D3.js
+++ W2D3.js
@@ -4,6 +4,13 @@
 isTallEnough(72); //true
 isTallEnough(28); //false
 
+function isTallEnough(number) {
+    if (number >= 48) {
+        return true;
+    }
+    return false;
+}
+
 
 //2- Write a function isGoodFreethrowShooter that takes a shooting percentage
 // (a number you can assume will be between 0 and 1 -- exclusive of 1).
@@ -22,3 +29,15 @@ isTallEnough(28); //false
 isGoodFreethrowShooter(0.90); //"Great freethrow shooter"
 isGoodFreethrowShooter(0.09); //"Horrible freethrow shooter"
 isGoodFreethrowShooter(0.75); //"Decent freethrow shooter"
+
+function isGoodFreethrowShooter(num) {
+    if (num >= 0 && num < 0.65) {
+        return "Horrible freethrow shooter";
+    }
+    else if (num >= 0.65 && num < 0.80) {
+        return "Decent freethrow shooter";
+    }
+    else if (num >= 0.80 && num < 1.00) {
+        return "Great freethrow shooter";
+    }
+}
