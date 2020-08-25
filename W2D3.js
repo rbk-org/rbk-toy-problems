8c9a6d6a84e1fcc91a54232258001c54209ec255
meenaFogah <yasmeenfoqha2001@gmail.com>
2020-08-24 21:35:07 -0700

    W2D3

--- W2D3.js
+++ W2D3.js
@@ -1,13 +1,19 @@
 //1- Write a function isTallEnough that takes a height (number) in inches, 
 //and returns true if the input is greater than or equal to 48, and false if it's less than 48 inches.
 //Calling your function should result in:
+function isTallEnough(hight){
+	var high = 48 
+	if( hight > 48 ) {
+		return true
+	}
+  return false 
+}
 isTallEnough(72); //true
 isTallEnough(28); //false
 
-
 //2- Write a function isGoodFreethrowShooter that takes a shooting percentage
-// (a number you can assume will be between 0 and 1 -- exclusive of 1).
-//and returns based on the following criteria:
+// (a number you c
+//and returns based on the following criteria:an assume will be between 0 and 1 -- exclusive of 1).
 // If the number is between 0-0.65, return "Horrible freethrow shooter"
 // If the number is between 0.65-0.80 return "Decent freethrow shooter"
 // If the number is between 0.80-1.00 return "Great freethrow shooter"
@@ -18,7 +24,19 @@ isTallEnough(28); //false
 //(this is what the original problem statement means by "exclusive of 1").
 
 // Calling your function should result in:
+function isGoodFreethrowShooter(x){
+if (x>=0 && x<0.65)
+{
+	return "Horrible freethrow shooter"
+}
+if (x>=0.65&&x<0.80 ){
+	return "Decent freethrow shooter"
+}
+if (x>=0.80&&x<1.00  ){
+	return "Great freethrow shooter"
 
+}
+}
 isGoodFreethrowShooter(0.90); //"Great freethrow shooter"
 isGoodFreethrowShooter(0.09); //"Horrible freethrow shooter"
 isGoodFreethrowShooter(0.75); //"Decent freethrow shooter"
