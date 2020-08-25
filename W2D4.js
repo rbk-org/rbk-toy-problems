afc779d25350cdd028f333a0d0798cd651da4f5c
RamiNawahda <rami.nawahda@gmail.com>
2020-08-25 09:43:23 +0200

    W2D4 RamiN toyProblem

--- W2D4.js
+++ W2D4.js
@@ -1,16 +1,50 @@
 //1.Write a function called sameLength that takes two strings as parmeters, 
 //and returns true if those strings have the same length, and false otherwise.
+function sameLength(string1,string2){
+	if(string1.length === string2.length){
+		return true;
+	} else{
+		return false;
+	} 
+}
 
 //2.Write a function called passwordLongEnough that takes a 'password' as a parameter 
 //and returns true if that password is long enough -- you get to decide what constitutes long enough.
 
+function passwordLongEnough(password){
+	if(password.length >= 8){
+		return true;
+	} else {
+		return "Please enter password long enogh";
+	}
+}
 //3.Write a function called rentalCar that takes a person's name and age as parmeters, 
 //and returns either 'You cannot have the keys, .', or "Have fun driving", 
 // depending on whether or not the person is old enough. 
 //In the US, most rental car companies do not allow you to rent a car until you are 21.
-
+function rentalCar(personName,personAge){
+	if(personAge > 21 ){
+		return personName+" Have fun driving";
+	}	else {
+		return personName+' You cannot have the keys';
+	} 
+} 
 // 4.Write a function called max that takes two numbers as parameters, 
 //and returns the larger one.
+function max(num1,num2){
+	if(num2 > num1){
+		return num2;
+	} else {
+		return num1;
+	}
+}
 
 // 5.Write a function called  min that takes two numbers as parameters,
 // and returns the smaller one.
+function min(num1,num2){
+	if(num2 < num1){
+		return num2;
+	} else {
+		return num1;
+	}
+}
