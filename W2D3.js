a2bd19f02237a44fe3f652785a710ebce8cec5d8
Nourasqar <noorashqar123@gmail.com>
2020-08-25 17:30:35 +0300

    w2d4 done

--- W2D3.js
+++ W2D3.js
@@ -1,6 +1,13 @@
 //1- Write a function isTallEnough that takes a height (number) in inches, 
 //and returns true if the input is greater than or equal to 48, and false if it's less than 48 inches.
 //Calling your function should result in:
+function isTallEnough(number){
+	if(number>=48){
+		return(true)
+	}else{
+		return(false)
+	}
+}
 isTallEnough(72); //true
 isTallEnough(28); //false
 
@@ -16,7 +23,17 @@ isTallEnough(28); //false
 //and 0.80 is considered a "Great freethrow shooter" (not "Decent freethrow shooter"). 
 //Additionally, you can get a number as high as 0.99, but never 1 
 //(this is what the original problem statement means by "exclusive of 1").
-
+function isGoodFreethrowShooter (number){
+	if(number>=0 && number<0.65){
+		return("Horrible freethrow shooter")
+	}else if(number>=0.65 && number<0.80){
+		return("Decent freethrow shooter")
+	}else if(number>=0.80 && number<1){
+		return("Great freethrow shooter")
+	}else {
+		return("error")
+	}
+}
 // Calling your function should result in:
 
 isGoodFreethrowShooter(0.90); //"Great freethrow shooter"
