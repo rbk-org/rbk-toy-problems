ff89561762e5919ce2d6b23b1e253a0b0f01444d
shaima96 <141062@ppu.edu.ps>
2020-08-26 23:30:00 +0300

    Solutions

--- W2D4.js
+++ W2D4.js
@@ -1,16 +1,103 @@
 //1.Write a function called sameLength that takes two strings as parmeters, 
 //and returns true if those strings have the same length, and false otherwise.
 
+function sameLength(str1,str2)
+{
+  if(str1.length === str2.length)
+  {
+    return true;
+  }
+  return false;
+}
+
+sameLength('shaima','azmi')
+
+
+
+
 //2.Write a function called passwordLongEnough that takes a 'password' as a parameter 
 //and returns true if that password is long enough -- you get to decide what constitutes long enough.
+
+
+function passwordLongEnough(password)
+{
+  if(password.length >8)
+  {
+    return true;
+  }
+  return false;
+}
+passwordLongEnough('shhhjkknknk')
+
+
+
+
 
 //3.Write a function called rentalCar that takes a person's name and age as parmeters, 
 //and returns either 'You cannot have the keys, .', or "Have fun driving", 
 // depending on whether or not the person is old enough. 
 //In the US, most rental car companies do not allow you to rent a car until you are 21.
 
+
+function rentalCar(name,age)
+{
+  if(age >= 21)
+  {
+    return " Have fun driving " + name;
+  }
+  return "You cannot have the keys";
+}
+
+rentalCar('shaima',30)
+
+
+
 // 4.Write a function called max that takes two numbers as parameters, 
 //and returns the larger one.
 
+function max(num1,num2)
+{
+  var min=Math.min(num1,num2);
+  var max=Math.max(num1,num2);
+  if(min >= max)
+  {
+  return min;
+  }
+  return max;
+}
+max(1,2)
+
+
+
+
+function max(num1,num2)
+{
+  var max=num2;
+  if(num2 <= num1)
+  {
+  max=num1;
+  }
+  return max;
+}
+max(1,2)
+
+
+
+
+
+
 // 5.Write a function called  min that takes two numbers as parameters,
 // and returns the smaller one.
+
+function min(num1,num2)
+{
+  var min=Math.min(num1,num2);
+  var max=Math.max(num1,num2);
+  if(min <= max)
+  {
+  return min;
+  }
+  return max;
+}
+min(1,2)
+
