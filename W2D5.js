68d80798c2298e0ba04699f9c8d1603d69d872d4
HanadiSabarnah <hsabarnah@gmail.com>
2020-08-26 10:18:53 +0300

    W2D5

--- W2D5.js
+++ W2D5.js
@@ -22,9 +22,20 @@ and you invoke the function with your guess -- if you and the function are think
  then, answer the following questions: 
  At present, the guess should be between 0 and 5. 
  We can think of 5 as the upper bound of the guess. 
- How many times is the upper bound repeated? 
+ How many times is the upper bound repeated?
+
+       infinity  times
+
  What if we wanted to change the upper bound to 6? 
+
+ if (n > 6) { 
+ return 'Out of bounds! Please try a number between 0 and 6.'; 
+ } else if (n === randInt(6))
+
+
  How many changes would be required? 
+
+
  Create a variable called upperBound to hold the upper bound, and then reference it instead of the number 5. 
  If you were asked to change the upper bound to some other number (e.g. 7), you should only have to make one change. 
  Modify guessMyNumber so that if the guess is incorrect, guessMyNumber includes the correct guess in its output, 
@@ -55,9 +66,29 @@ include in the message something that indicates that a new high score has been s
 You may want to increase the upperBound on the guess.
 
 .All of the following exercises involve augmenting the guessMyNumber function.
-/*
+
 
 // Your code is here
+*/
+
+function guessMyNumber(n, upperBound) {
+  
+  if (n > upperBound) {
+    return "Out of bounds! Please try a number between 0 and " + upperBound;
+  } else if (n === randInt(upperBound)) {
+    return "You guessed my number in" + n + " guess." "/n" + "Congratulations! You guessed my number on the first try!";
+
+    guessMyNumber(n, upperBound + 1)
+
+  }
+  return "Nope! That wasn't it! The correct guess was: " + randInt(upperBound);
+  guessMyNumber(n, upperBound - 1)
+}
+
+function randInt(n) {
+  return Math.floor(Math.random() * (n + 1))
+}
+
 
 
 
