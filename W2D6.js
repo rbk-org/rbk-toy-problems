5189be6f726ec2036e5733f54d771ee49d2de121
Halasalhab <halasalhab0@gmail.com>
2020-08-27 09:59:06 +0300

    solved *-*

--- W2D6.js
+++ W2D6.js
@@ -23,10 +23,13 @@ loopAString('abcd');
 
 */
 function loopAString(string) {
-  // create an index variable
-  // create a loop which iterates over the input string
-  // log current string character to the console
-  // increment value of index variable
+	var i=0;
+	while(i<string.length){
+		console.log(string[i]);
+		string.slice(1);
+		i++;
+	}
+ 
 }
 
 
@@ -35,6 +38,21 @@ function loopAString(string) {
 //followed by a number starting from zero(solve it using while loop).
 // reverseStr('hello'); "o1l2l3e4h"
 // Your code is here
-
+function reverseStr(str){
+	var i=str.length-1;
+	var t=0;
+  var y="";
+	while(i>=0){
+		while(t<str.length){
+y=y+t+str[i];
+t++;
+i--;
+    }
+    
+    
+    }
+    return y;
+    }
+reverseStr("hala")
 
 
