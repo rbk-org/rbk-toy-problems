5082741b00269350f2b630386dc75efe5cf38f61
NajwanQandeel <najwan.qandeel@gmail.com>
2020-08-27 18:20:02 +0300

    solution of W2D6

--- W2D6.js
+++ W2D6.js
@@ -4,30 +4,22 @@ and logs all of its characters (one at a time) to the console.
 Your function should use a while loop to log each character 
 from the beginning to the end of the string, then return nothing. 
 Below is an example of the code running.
-
-loopAString('nodeJS');
-// console output:
-  // n
-  // o
-  // d
-  // e
-  // J
-  // S
-
 loopAString('abcd');
 // console output:
   // a
   // b
   // c
   // d
-
 */
+
 function loopAString(string) {
-  // create an index variable
-  // create a loop which iterates over the input string
-  // log current string character to the console
-  // increment value of index variable
+  var i =0;
+  while(i< string.length){
+  	console.log(string[i])
+  	i++;
+  }
 }
+loopAString('abcd');
 
 
 // 2-Write a function called reverseStr that takes a string as an parameter 
@@ -35,6 +27,16 @@ function loopAString(string) {
 //followed by a number starting from zero(solve it using while loop).
 // reverseStr('hello'); "o1l2l3e4h"
 // Your code is here
+function reverseStr(string){
+  var str='';
+  var i = 0;
+  while(i <= string.length){
+    //str += string[string.length-i];
+    console.log(string[string.length-i],i-1)
+    i++;
+  }
+}
+reverseStr('hello')
 
 
 
