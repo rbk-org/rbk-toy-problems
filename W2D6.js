ea449466665734e8eaf5e7f8f7d90c9ffc4e7749
Qamar19 <48962147+Qamar19@users.noreply.github.com>
2020-08-27 09:26:35 +0300

    W2D6

---
+++ W2D6.js
@@ -0,0 +1,59 @@
+/*
+1-Write a function called loopAString that takes one parameter, a string of characters, 
+and logs all of its characters (one at a time) to the console. 
+Your function should use a while loop to log each character 
+from the beginning to the end of the string, then return nothing. 
+Below is an example of the code running.
+
+loopAString('nodeJS');
+// console output:
+  // n
+  // o
+  // d
+  // e
+  // J
+  // S
+
+loopAString('abcd');
+// console output:
+  // a
+  // b
+  // c
+  // d
+
+*/
+function loopAString(string) {
+  // create an index variable
+  var index = 0
+  // create a loop which iterates over the input string
+  while (index < string.length) {
+    // log current string character to the console
+    console.log(string[index])
+    // increment value of index variable
+    index++
+  }
+}
+
+
+// 2-Write a function called reverseStr that takes a string as an parameter 
+//and returns the reverse of each letter 
+//followed by a number starting from zero(solve it using while loop).
+// reverseStr('hello'); "o1l2l3e4h"
+// Your code is here
+
+
+function reverseStr(str) {
+  var i = str.length - 1
+  var newstr = ''
+  while (i > 0) {
+    newstr += str[i] + (str.length - i)
+    i--
+
+  }
+  newstr += str[(str.length - 1)]
+
+  return newstr
+}
+
+
+
