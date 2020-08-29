869773b91d9f984c441a8eb8ed4cb7407eba1eb1
Baraa <b.jayousi98@gmail.com>
2020-08-29 03:25:44 +0300

    Not complete

--- W2D5.js
+++ W2D5.js
@@ -5,27 +5,46 @@ and you invoke the function with your guess -- if you and the function are think
  you win! Otherwise, the function informs you that your guess was incorrect. 
  A version of this game might look like this (the randInt function is included for convenience):
 */
- function guessMyNumber(n) { 
- if (n > 5) { 
- return 'Out of bounds! Please try a number between 0 and 5.'; 
- } else if (n === randInt(5)) { 
- return 'You guessed my number!'; 
+ function guessMyNumber(n) 
+ { 
+	 if (n > 5) { 
+	 return 'Out of bounds! Please try a number between 0 and 5.'; 
+	 }
+	 else if (n === randInt(5)) 
+	 { 
+	 return 'You guessed my number!'; 
+	 } 
+	 return "Nope! That wasn't it!"; 
  } 
- return "Nope! That wasn't it!"; 
- } 
- function randInt(n) { 
+ 
+ function randInt(n) 
+ { 
  return Math.floor(Math.random() * (n + 1)) 
  }
  /*
  Read and test both of the functions in your console.
  and then invoke the functions from the console) and affirm that you understand how they work; 
  then, answer the following questions: 
- At present, the guess should be between 0 and 5. 
+ 
+ 
+ At present, the guess should be between 0 and 5.  
  We can think of 5 as the upper bound of the guess. 
+ 
+ 
  How many times is the upper bound repeated? 
+ 
+ 4 times
+ 
  What if we wanted to change the upper bound to 6? 
+ 
+ considering the giving function we could change the value of (n) while invoking randInt()
+ 
  How many changes would be required? 
+ 
+ just the value of one passed parameter
+ 
  Create a variable called upperBound to hold the upper bound, and then reference it instead of the number 5. 
+ 
  If you were asked to change the upper bound to some other number (e.g. 7), you should only have to make one change. 
  Modify guessMyNumber so that if the guess is incorrect, guessMyNumber includes the correct guess in its output, 
   e.g. 'Nope! The correct number was: X' (where X would have been the correct number).
@@ -55,7 +74,7 @@ include in the message something that indicates that a new high score has been s
 You may want to increase the upperBound on the guess.
 
 .All of the following exercises involve augmenting the guessMyNumber function.
-/*
+*/
 
 // Your code is here
 
