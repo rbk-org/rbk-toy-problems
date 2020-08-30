eae9fb68ee693c8c0602eea3cdbf6d91b9b65297
GitHub-MahmoudWaked <mahmoud_waked@hotmail.com>
2020-08-30 10:01:43 +0300

    done!

--- W3D2.js
+++ W3D2.js
@@ -8,6 +8,14 @@ countNumOfStrings(["foo", 2, 10, "bar", 5, false, {}, "baz"]); //3
 countNumOfStrings(["There is only one string in this example"]); //1
 Hint: Recall typeof? If not: check it out on MDN. :-)
 */
+function countNumOfStrings(arr){
+  var count = 0
+  if(arr.type() === string){
+  count++
+  }
+  return count
+}
+console.log(countNumOfStrings(["hello", "world", 5, true, {}]))
 /*
 2- Write a function called listLengthOfAllWords that takes an array
 of words (strings),
@@ -16,6 +24,12 @@ Calling your function should result in:
 listLengthOfAllWords(['hello', 'world', 'I', 'can', 'code']); //[5, 5,
 1, 3, 4];
 */
+function listLengthOfAllWords(arr){
+  var num = []
+  for(var i = 0; i<arr.length;i++){
+    num[i] = arr[i].length
+  }
+  return num
 /*
 Write a function called flipPairs that takes a string a parameter
 and returns the given string after Flip every pair of characters.
@@ -25,3 +39,11 @@ interesting!';
 var output = flipPairs(input);
 console.log(output); // --> hcce kuo toh wnietertsni ghtsip orlbmei
 ,si 't sniasenyli tnreseitgn!*/
+function flipPairs(input){
+  var output = " "
+  for (var i = 0; i < input.length; i--){
+    output = output + input.charAt(i)
+  }
+  return output
+}
+console.log(flipPairs("check out how interesting this problem is, it\'s insanely"))
