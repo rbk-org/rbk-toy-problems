3d47dd7b501ff0c6de1596d4acd673e16e1f927c
israakittaneh <israakittanehm@gmail.com>
2020-08-30 09:40:58 +0300

    W3D2israa

--- W3D2.js
+++ W3D2.js
@@ -7,8 +7,9 @@ countNumOfStrings(["hello", "world", 5, true, {}]); //2
 countNumOfStrings(["foo", 2, 10, "bar", 5, false, {}, "baz"]); //3
 countNumOfStrings(["There is only one string in this example"]); //1
 Hint: Recall typeof? If not: check it out on MDN. :-)
-*/
+
 /*
+
 2- Write a function called listLengthOfAllWords that takes an array
 of words (strings),
 and returns an array of numbers representing the length of each word.
@@ -25,3 +26,32 @@ interesting!';
 var output = flipPairs(input);
 console.log(output); // --> hcce kuo toh wnietertsni ghtsip orlbmei
 ,si 't sniasenyli tnreseitgn!*/
+
+//Ex1
+function countNumOfStrings(array){
+	var count = 0;
+	for(var i = 0 ; i < array.length ; i++){
+		if(typeof(array[i])=== 'string'){
+			count++;
+		}
+	}return count;
+}
+
+//Ex2
+function listLengthOfAllWords(array){
+	var arr= [];
+	for(var i = 0 ; i < array.length ; i++){
+		arr.push(array[i].length);
+
+	}return arr;
+}
+
+//Ex3
+
+function flipPairs(input){
+	var output = '';
+	for(var i = 0 ; i< input.length ; i+=2){
+		var result = output.concat(input[i+1]);
+
+	}return result;
+}
