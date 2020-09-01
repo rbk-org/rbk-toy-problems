ded3992aceaf13e7b0da645c2087f6dad95bf76e
NajwanQandeel <najwan.qandeel@gmail.com>
2020-09-01 13:31:05 +0300

    w3d4

--- W3D4.js
+++ W3D4.js
@@ -1,9 +1,13 @@
 /*
 1-Write a function called tallEnoughToRide  that takes an array of people objects, 
 and returns a an array of names of people who are greater than or equal to 48 inches in height.
-
-You can assume an input which looks like this:
-
+Calling your function should result in:
+tallEnoughToRide(groupA); //["Mia", "Kiana", "Alex"];
+Remember: your function should work off of the data it receives, 
+not a global variable or only the specific example above. 
+I.e. If someone called your function with 10 objects 
+(with all safely have the same properties), it should work just as well.
+*/
 var groupA = [
   {
     name: "Mia",
@@ -26,16 +30,19 @@ var groupA = [
     heightInInches: 48
   }
 ]
-Calling your function should result in:
-tallEnoughToRide(groupA); //["Mia", "Kiana", "Alex"];
 
-Remember: your function should work off of the data it receives, 
-not a global variable or only the specific example above. 
-I.e. If someone called your function with 10 objects 
-(with all safely have the same properties), it should work just as well.
-*/
+function tallEnoughToRide(initial){
+  var arrayOfPeople = initial;
+  var names=[];
+ 
+   
+      for(var i=0;i<arrayOfPeople.length;i++){
+     if(arrayOfPeople[i]['heightInInches'] >= 48){
+      names.push(arrayOfPeople[i].name)
+    }
+      } return names;
+   }
 
-// your answer is here
 
 /*
 2-Working off of the same data structure as tallEnoughToRide, 
@@ -46,8 +53,16 @@ along with how tall they are (see sample input below).
 Calling your function should result in:
 
 tallestPerson(groupA); //"Kiana at 55 inches"
-
-
 */
 
-// your answer is here
+function tallestPerson(initial){
+  var arrayOfPeople = initial;
+  var theTallest = '';
+  theTallest = arrayOfPeople[0]['heightInInches'];
+  for(var i=0;i< arrayOfPeople.length;i++){
+    if(theTallest > arrayOfPeople[i][heightInInches]){
+      theTallest = arrayOfPeople[0][heightInInches];
+      return theTallest;
+    }
+  }
+}
