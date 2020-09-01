9d835c28aa74a1df253360762d64c166cce7a97f
HanadiSabarnah <hsabarnah@gmail.com>
2020-09-01 09:58:56 +0300

    W3D4

--- W3D4.js
+++ W3D4.js
@@ -37,6 +37,46 @@ I.e. If someone called your function with 10 objects
 
 // your answer is here
 
+var groupA=[];
+
+function createStudent(name,age,heightInInches)
+ {
+ 	var obj={}
+ 	obj.name=name
+ 	obj.age=age
+ 	obj.heightInInches=heightInInches
+ 	return obj
+
+ }
+ var obj1 ={ name: "Mia", age: 10,heightInInches: 52 };
+ var obj2= {name: "Jaya",age: 9,heightInInches: 45};
+ var obj3 ={name: "Kiana",age: 10,heightInInches: 55};
+  var obj4 ={name: "Alex",age: 11,heightInInches: 48};
+
+
+ function addstudent(obj){
+ 	groupA.push(obj)
+ }
+ 
+
+
+
+  function tallEnoughToRide(array){
+	
+for (var i = 0; i < array.length; i++) {
+		if (array[i]['heightInInches'] <48) {
+			array.splice(i, 1)
+		}
+	}
+		return array[i]['name']
+	}
+
+
+
+
+
+
+
 /*
 2-Working off of the same data structure as tallEnoughToRide, 
 write a function called tallestPerson that takes  an array of people objects as well, 
@@ -51,3 +91,41 @@ tallestPerson(groupA); //"Kiana at 55 inches"
 */
 
 // your answer is here
+
+var groupA=[];
+
+function createStudent(name,age,heightInInches)
+ {
+ 	var obj={}
+ 	obj.name=name
+ 	obj.age=age
+ 	obj.heightInInches=heightInInches
+ 	return obj
+
+ }
+ var obj1 ={ name: "Mia", age: 10,heightInInches: 52 };
+ var obj2= {name: "Jaya",age: 9,heightInInches: 45};
+ var obj3 ={name: "Kiana",age: 10,heightInInches: 55};
+  var obj4 ={name: "Alex",age: 11,heightInInches: 48};
+
+
+ function addstudent(obj){
+ 	groupA.push(obj)
+ }
+ 
+
+
+
+  function tallEnoughToRide(array){
+
+  	var heightest = array[0]
+	
+for (var i = 1; i < array.length; i++) {
+		if (array[i]['heightInInches'] >array[0]) {
+			heightest= array[i]['heightInInches']
+		}
+	}
+		return array[i]['name']
+	}
+
+
