71512ff2f359749ae7e68c3518f3d1604a9c8d84
fdo2 <fdo2@live.com>
2020-09-02 09:33:51 +0300

    reload W3D2.js W3D3.js W3D4.js

---
+++ W3D2.js
@@ -0,0 +1,72 @@
+/*
+1- Write a function called countNumOfStrings that takes an array of
+mixed elements (different data types)
+and returns the number of strings there are in the array.
+Calling your function should result in:
+countNumOfStrings(["hello", "world", 5, true, {}]); //2
+countNumOfStrings(["foo", 2, 10, "bar", 5, false, {}, "baz"]); //3
+countNumOfStrings(["There is only one string in this example"]); //1
+Hint: Recall typeof? If not: check it out on MDN. :-)
+*/
+/*
+2- Write a function called listLengthOfAllWords that takes an array
+of words (strings),
+and returns an array of numbers representing the length of each word.
+Calling your function should result in:
+listLengthOfAllWords(['hello', 'world', 'I', 'can', 'code']); //[5, 5,
+1, 3, 4];
+*/
+/*
+Write a function called flipPairs that takes a string a parameter
+and returns the given string after Flip every pair of characters.
+Example:
+var input = 'check out how interesting this problem is, it\'s insanely
+interesting!';
+var output = flipPairs(input);
+console.log(output); // --> hcce kuo toh wnietertsni ghtsip orlbmei
+,si 't sniasenyli tnreseitgn!*/
+
+
+//1
+	function countNumOfStrings(arr){
+		var sNum = 0;
+		for(var i=0; i<arr.length; i++){
+	   	 if(typeof (arr[i]) === 'string'){
+	       sNum++
+	    }
+  	}
+	    return sNum
+	}
+ countNumOfStrings(["foo", 2, 10, "bar", 5, false, {}, "baz"])
+
+
+
+
+ //2
+ 	
+ 	function listLengthOfAllWords(arr){
+ 		var sLength =[];
+ 		for(var i=0; i<arr.length; i++){
+ 		    sLength.push(arr[i].length);
+     }
+          return  sLength;
+ 	}
+ 	listLengthOfAllWords(['hello', 'world', 'I', 'can', 'code'])
+
+
+//3
+	function  flipPairs(str) {
+		var strr="";
+		var i;
+	for (i=1; i<str.length; i+=2){
+		strr+= str[i] + str[i-1]
+	}
+	if (str[i-1]!== undefined){
+		strr+= str[i-1]
+	}
+	return strr		
+	}
+	flipPairs("hello world") 
+
+
+	
