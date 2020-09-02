62b1f97e4db890932cb29f4878ae78e2fa58b03c
dimaOwens <dimaOwens>
2020-09-02 09:24:41 +0300

    solved

--- W3D5.js
+++ W3D5.js
@@ -1,10 +1,11 @@
+  
 /*
 You were put in charge of ordering for tonight's company get-together, 
 and you were given a data set with people's meal preferences and dietary restrictions. 
 Write a function called orderAVegetarianDish that takes  an array of empoloyee objects, 
 and returns true if at least 1 person is listed "vegetarian" on their "mealPreferences". 
 Otherwise, your function should return false.
-
+*/
 var staffA = [
   {
     name: "Lia",
@@ -31,9 +32,19 @@ var staffA = [
     mealPreferences: "non-vegetarian"
   }
 ];
-Calling your function should result in:
 
-orderAVegetarianDish(staffA); //true
-*/
 
-// your answer is here 
+
+function orderAVegetarianDish(staff){
+
+  for(var i = 0; i < staff.length; i++){
+    if(staff[i]['mealPreferences'] === 'vegetarian'){
+      return true;
+    }
+  }
+  return false;
+}
+
+console.log(orderAVegetarianDish(staffA));
+
+
