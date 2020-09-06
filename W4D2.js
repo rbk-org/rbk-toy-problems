99dd13f21c423b066ce2daf636161c8aa33eaa9f
imadaqel <emadaqel92@gmail.com>
2020-09-06 10:21:25 +0300

    origin

--- W4D2.js
+++ W4D2.js
@@ -15,3 +15,58 @@
 // Write a function called sum that accepts two numbers as parameters, and sum them together but without suming them togther directly
 //you can only add one at each summation, you'll need to use recursion in this.
 
+  //part 2 q2
+
+
+function increase(x){
+  x+1;
+
+}
+
+function decrease(x){
+
+ x-1;
+
+}
+
+function sum(x,y){
+   if(y>x){
+   if(y===0){ return x;}
+
+
+
+  else{ return increase(y);decrease(x);}}
+
+   else {
+    if (x===0){return y;}
+
+    else {return increase(x);decrease(y);}} 
+
+   }  
+
+
+//part 1
+function classmate(mate,name,age,iq,length){
+  
+    mate=new Object();
+   mate.name=name;
+   mate.age=age;
+   mate.iq=iq;
+   mate.length=length;
+   //array.push(mate)
+   console.log(displayfriend(mate))
+   return mate
+   
+  }
+   
+   
+  classmate("std1","mh",20,255,170);
+  classmate("std2","mk",20,220,170);
+  classmate("std2","msssss",20,220,170);
+
+
+function displayfriend(mate){
+
+  return mate.name+ " "+ mate.age;
+
+}
