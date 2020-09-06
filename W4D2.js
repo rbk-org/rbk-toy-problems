7be3e35c91ecd9e8a8cc914062c4d612c04158b6
Yasir-Tbaileh <yasur96@gmail.com>
2020-09-06 10:02:59 +0300

    W2D4 solved by yasir

--- W4D2.js
+++ W4D2.js
@@ -9,9 +9,59 @@
 // 	-write a function called addFriend that takes a mate as an argument and add it to you classMates arraya.
 // 	-calculate the number of male friends that your class have by writing a function called nbOfMale.
 
+function ClassMate(name, age, gender, hobby) {
+    var x = {};
+    x.name = name;
+    x.age = age;
+    x.gender = gender;
+    x.hobby = hobby;
+    x.displayFriend = displayFriend;
+    x.addFriend = addFriend;
+    return x;
+}
+var classMates = [];
+
+function displayFriend(mate){
+    var str = "";
+    for (var key in mate){
+        str += `${key}: ${mate[key]} \n`;
+    }
+    return str
+}
+
+function addFriend(mate){
+    classMates.push(mate);
+}
+
+function nbOfMale(classMates){
+    var numOfMale = 0;
+    for (var i = 0; i < classMates.length; i++){
+        for (var key in classMates[i]){
+            if (classMates[i][key].toLowerCase() === "male"){
+                numOfMale += 1;
+            }
+        }
+    }
+    return numOfMale;
+}
+
+
+var x = ClassMate("ahmad", 25, "male", "sport");
+
 
 //Part2:
 // Using recursion Write a JavaScript function to find the greatest common divisor (gcd) of two positive numbers.
+function gcd(num1, num2) {
+    if (num1 % num2 == 0) {
+        return num2;
+    }
+    return gcd(num2, num1 % num2)
+}
 // Write a function called sum that accepts two numbers as parameters, and sum them together but without suming them togther directly
 //you can only add one at each summation, you'll need to use recursion in this.
-
+function add(num1, num2) {
+    if (num2 == 0) {
+        return num1;
+    }
+    return add(num1, num2 - 1) + 1
+}
