9bc92972fc49ca6991d466d4892f2f062bccb898
asemnouri <64498291+asemnouri@users.noreply.github.com>
2020-09-07 10:10:26 +0300

    W4D3

--- _W4D3.js
+++ _W4D3.js
@@ -6,10 +6,17 @@ The nutritionalValue is also an object, which has the following properties: lowS
 Your function should add the prices of all the items in the array if the item is true for both lowSugar and lowSodium
 
 Finally, your function should return the sum total of all the healthy items
-
+*/
 function onlyPayForHealthyThings(foodList) {
-
+  var sum =0
+for(var i=0;i<foodList.length;i++){
+  if(foodList[i]['nutritionalValue']['lowSugar']===true && foodList[i]['nutritionalValue']['lowSodium']===true){
+    sum+=foodList[i]['price']
+  }
 }
+return sum
+}
+onlyPayForHealthyThings(myCart)
 var myCart = [ 
   { name: 'chips',
     nutritionalValue: { lowSugar: true, lowSodium: false },
@@ -29,6 +36,6 @@ var myCart = [
   { name: 'avocados',
     nutritionalValue: { lowSugar: true, lowSodium: true },
     price: 0.75 } 
-  ];
+  ];/*
 Calling onlyPayForHealthyThings(myCart) should return 2.85 from adding the prices of "carrots", "apples", and "avocados"
 */
