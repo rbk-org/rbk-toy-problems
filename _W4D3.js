eb8816282bf73dcb9ed5e33d5be91b90cdba4f26
rzanabusamra22 <rzanabusamra@gmail.com>
2020-09-07 09:28:39 +0300

    solved and checked

--- _W4D3.js
+++ _W4D3.js
@@ -32,3 +32,34 @@ var myCart = [
   ];
 Calling onlyPayForHealthyThings(myCart) should return 2.85 from adding the prices of "carrots", "apples", and "avocados"
 */
+var myCart = [ 
+  { name: 'chips',
+    nutritionalValue: { lowSugar: true, lowSodium: false },
+    price: 0.75 },
+  { name: 'carrots',
+    nutritionalValue: { lowSugar: true, lowSodium: true },
+    price: 1.5 },
+  { name: 'cookies',
+    nutritionalValue: { lowSugar: false, lowSodium: true },
+    price: 2.5 },
+  { name: 'apples',
+    nutritionalValue: { lowSugar: true, lowSodium: true },
+    price: 0.6 },
+  { name: 'soda',
+    nutritionalValue: { lowSugar: false, lowSodium: true },
+    price: 1.1 },
+  { name: 'avocados',
+    nutritionalValue: { lowSugar: true, lowSodium: true },
+    price: 0.75 } 
+  ];
+  
+ function onlyPayForHealthyThings (arrayOfObjects){
+	var countHealthy=0
+	for (var i=0; i<arrayOfObjects.length; i++){
+		if(arrayOfObjects[i].nutritionalValue.lowSugar === true && arrayOfObjects[i].nutritionalValue.lowSodium === true){
+        countHealthy+=arrayOfObjects[i].price	
+		}
+	}
+	return countHealthy;
+}
+ onlyPayForHealthyThings (myCart);
