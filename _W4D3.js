fa2034a57c945701cdac7f096a61da2785390ef0
hibtmimi <hibatamimi24@gmail.com>
2020-09-07 10:39:47 +0300

    done

--- _W4D3.js
+++ _W4D3.js
@@ -1,34 +1,50 @@
-/*
-Write a function called onlyPayForHealthyThings, that takes an array of objects.
 
-Each object is a grocery item with the following properties: name, nutritionalValue, price.
-The nutritionalValue is also an object, which has the following properties: lowSugar, lowSodium
-Your function should add the prices of all the items in the array if the item is true for both lowSugar and lowSodium
+// Write a function called onlyPayForHealthyThings, that takes an array of objects.
 
-Finally, your function should return the sum total of all the healthy items
+// Each object is a grocery item with the following properties: name,p nutritionalValue, Price.
+// The nutritionalValue is also an object, which has the following properties: lowSugar, lowSodium
+// Your function should add the prices of all the items in the array if the item is true for both lowSugar and lowSodium
 
-function onlyPayForHealthyThings(foodList) {
+// Finally, your function should return the sum total of all the healthy items
+// input : myCart
+// output : sum of total price
 
-}
+
 var myCart = [ 
   { name: 'chips',
     nutritionalValue: { lowSugar: true, lowSodium: false },
     price: 0.75 },
-  { name: 'carrots',
-    nutritionalValue: { lowSugar: true, lowSodium: true },
-    price: 1.5 },
+  // { name: 'carrots',
+  //   nutritionalValue: { lowSugar: true, lowSodium: true },
+  //   price: 1.5 },
   { name: 'cookies',
     nutritionalValue: { lowSugar: false, lowSodium: true },
     price: 2.5 },
-  { name: 'apples',
-    nutritionalValue: { lowSugar: true, lowSodium: true },
-    price: 0.6 },
+  // { name: 'apples',
+  //   nutritionalValue: { lowSugar: true, lowSodium: true },
+  //   price: 0.6 },
   { name: 'soda',
     nutritionalValue: { lowSugar: false, lowSodium: true },
     price: 1.1 },
-  { name: 'avocados',
-    nutritionalValue: { lowSugar: true, lowSodium: true },
-    price: 0.75 } 
+  // { name: 'avocados',
+  //   nutritionalValue: { lowSugar: true, lowSodium: true },
+  //   price: 0.75 } 
   ];
-Calling onlyPayForHealthyThings(myCart) should return 2.85 from adding the prices of "carrots", "apples", and "avocados"
-*/
+// Calling onlyPayForHealthyThings(myCart) should return 2.85 from adding the prices of "carrots", "apples", and "avocados"
+
+function onlyPayForHealthyThings(array) {
+  var foodListPrice = []
+  var sum = 0
+  for(var i = 0 ; i< array.length ; i++){
+    if(array[i]["nutritionalValue"]["lowSugar"] === true && array[i]["nutritionalValue"]["lowSodium"]=== true){
+     foodListPrice.push(array[i]["price"])
+  }
+}
+
+for(var j = 0 ; j < foodListPrice.length ; j++){
+  sum = sum +foodListPrice[j]
+ 
+}
+ return sum
+    }
+    console.log(onlyPayForHealthyThings(myCart))
