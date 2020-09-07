17c37633e956e9eaed992ab007e80dc34c023bb1
shaima96 <141062@ppu.edu.ps>
2020-09-07 09:34:53 +0300

    W4D3

--- _W4D3.js
+++ _W4D3.js
@@ -32,3 +32,51 @@ var myCart = [
   ];
 Calling onlyPayForHealthyThings(myCart) should return 2.85 from adding the prices of "carrots", "apples", and "avocados"
 */
+
+
+
+
+var myCart = [ 
+  { name: 'chips',
+    nutritionalValue: { lowSugar: true, lowSodium: false },
+    price: 0.75 },
+  { name: 'carrots',
+    nutritionalValue: { lowSugar: true, lowSodium: true },
+    price: 1.5 },
+  { name: 'cookies',
+    nutritionalValue: { lowSugar: false, lowSodium: true },
+    price: 2.5 },
+  { name: 'apples',
+    nutritionalValue: { lowSugar: true, lowSodium: true },
+    price: 0.6 },
+  { name: 'soda',
+    nutritionalValue: { lowSugar: false, lowSodium: true },
+    price: 1.1 },
+  { name: 'avocados',
+    nutritionalValue: { lowSugar: true, lowSodium: true },
+    price: 0.75 } 
+  ];
+
+
+
+
+// Your function should add the prices of all the items in the array if the item is true for both lowSugar and lowSodium Finally, your function should return the sum total of all the healthy item
+
+function onlyPayForHealthyThings(foodList) {
+  var sum=0;
+ 
+  for(var i=0;i<foodList.length;i++)
+  {
+    if(foodList[i].nutritionalValue.lowSodium=== true && foodList[i].nutritionalValue.lowSugar === true)
+    {
+      sum+=foodList[i].price;
+    }
+  }
+ return sum;
+
+}
+
+
+
+
+onlyPayForHealthyThings(myCart)//return 2.85 from adding the prices of "carrots", "apples", and "avocados"
