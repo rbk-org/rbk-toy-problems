aba1f4961b40910a21da5fc67906e881ae78fa55
hibtmimi <hibatamimi24@gmail.com>
2020-09-08 09:52:02 +0300

    done

--- W4D4.js
+++ W4D4.js
@@ -4,45 +4,47 @@ but returns an object with the properties title, artist,
 and durationInSeconds. The values of each property is up to you, 
 and you can explicitly type out these values, 
 but these three properties should exist within the object your function returns.
-
-function myFavoriteSong() {
-
-}
-
-Calling your function should result in something like:
-
-myFavoriteSong(); //{title: "Countdown", artist: "Beyonce", durationInSeconds: 212}
-
 */
+function myFavoriteSong() {
+return {title:"Countdown" , artist:"Beyonce" , durationInSeconds: 212 }
+}
+console.log(myFavoriteSong())  //{title: "Countdown", artist: "Beyonce", durationInSeconds: 212}
+
+
 
 /*
 2-Write a function called nextTwoBusTimes that takes an array of numbers representing (in minutes) 
 when the busses come for the day. 
 Your function should return a string which denotes when the nearest bus arrives, and the one after that.
 
+*/
+
 function nextTwoBusTimes(timesArr) {
+return "The next 2 busses arrive in " + timesArr[0] + " and " + timesArr[1] + " minutes "
 
 }
-Calling your function should result in something like:
+//Calling your function should result in something like:
 
 var bus10Schedule = [12, 24, 35, 47, 60];
 nextTwoBusTimes(bus10Schedule); //"The next 2 busses arrive in 12 and 24 minutes."
-*/
 
 
-/*
-
-3-Write a function called nextAndLastBusTimes that takes an array of numbers representing (in minutes) 
-when the busses come for the day. 
-Your function should return a string which denotes when the nearest bus arrives, and the last for the day.
+// 3-Write a function called nextAndLastBusTimes that takes an array of numbers representing (in minutes) 
+// when the busses come for the day. 
+// Your function should return a string which denotes when the nearest bus arrives, and the last for the day.
 
 function nextAndLastBusTimes(timesArr) {
+  for(var i = 0 ; i < timesArr.length ; i++){
+	return "The next bus arrive in " + timesArr[0] + " and The last bus is in " + timesArr[i-1] + " minutes "
+  }
+
 
 }
-Calling your function should result in something like:
+
+//Calling your function should result in something like:
 
 var bus10Schedule = [12, 24, 35, 47, 60];
 nextAndLastBusTimes(bus10Schedule); //"The next bus is in 12 minutes. The last bus is in 60 minutes.
-*/
+
 
 
