e14f1c1e533ed757443c0cb6c55231034e64a1d3
rzanabusamra22 <rzanabusamra@gmail.com>
2020-09-08 09:31:36 +0300

    Done

--- W4D4.js
+++ W4D4.js
@@ -14,7 +14,16 @@ Calling your function should result in something like:
 myFavoriteSong(); //{title: "Countdown", artist: "Beyonce", durationInSeconds: 212}
 
 */
+function myFavoriteSong(){
+	var obj={
+		title:"Countdown",
+		artist:"Beyonce",
+		durationInSeconds:212
+	}
 
+	return obj 
+}
+ myFavoriteSong();
 /*
 2-Write a function called nextTwoBusTimes that takes an array of numbers representing (in minutes) 
 when the busses come for the day. 
@@ -28,8 +37,14 @@ Calling your function should result in something like:
 var bus10Schedule = [12, 24, 35, 47, 60];
 nextTwoBusTimes(bus10Schedule); //"The next 2 busses arrive in 12 and 24 minutes."
 */
-
-
+function nextTwoBusTimes(array){
+   var str=""; i=0;
+   while (i<array.length){
+   return str ="The next 2 busses arrives in"+" "+ array[i]+" and "+array[i+1]+" "+"minutes."
+   i++;
+   }
+}
+nextTwoBusTimes([12, 24, 35, 47, 60]);
 /*
 
 3-Write a function called nextAndLastBusTimes that takes an array of numbers representing (in minutes) 
@@ -44,5 +59,13 @@ Calling your function should result in something like:
 var bus10Schedule = [12, 24, 35, 47, 60];
 nextAndLastBusTimes(bus10Schedule); //"The next bus is in 12 minutes. The last bus is in 60 minutes.
 */
+function nextAndLastBusTimes(array){
+   var str=""; i=0;
+   while (i<array.length){
+   return str ="The next buss is in"+" "+ array[i]+" minutes. "+" The last bus is in "+array[array.length-1]+" "+"minutes."
+   i++;
+   }
+}
+nextAndLastBusTimes([12, 24, 35, 47, 60]);
 
 
