eb3dc488f9f34791dbea7ba5a7728b1124dff11b
imadaqel <emadaqel92@gmail.com>
2020-09-08 09:39:31 +0300

    origin

--- W4D4.js
+++ W4D4.js
@@ -3,13 +3,22 @@
 but returns an object with the properties title, artist, 
 and durationInSeconds. The values of each property is up to you, 
 and you can explicitly type out these values, 
-but these three properties should exist within the object your function returns.
+but these three properties should exist within the object your function returns.*/
 
 function myFavoriteSong() {
+ 
+  return {
+  	
+   title: "Countdown",
+    artist: "Beyonce",
+    durationInSeconds: 212
+  }
 
 }
 
-Calling your function should result in something like:
+myFavoriteSong()
+
+/*Calling your function should result in something like:
 
 myFavoriteSong(); //{title: "Countdown", artist: "Beyonce", durationInSeconds: 212}
 
@@ -29,6 +38,17 @@ var bus10Schedule = [12, 24, 35, 47, 60];
 nextTwoBusTimes(bus10Schedule); //"The next 2 busses arrive in 12 and 24 minutes."
 */
 
+   
+
+
+function nextTwoBusTimes(timesArr) {
+   
+   return "The next 2 busses arrive in " + timesArr[0] +' and ' + timesArr[1] + " minutes."
+    
+}
+var bus10Schedule = [12, 24, 35, 47, 60];
+nextTwoBusTimes(bus10Schedule);
+
 
 /*
 
@@ -45,4 +65,14 @@ var bus10Schedule = [12, 24, 35, 47, 60];
 nextAndLastBusTimes(bus10Schedule); //"The next bus is in 12 minutes. The last bus is in 60 minutes.
 */
 
+
+
+
+function nextAndLastBusTimes(timesArr) {
+   
+   return "The next 2 busses arrive in " + timesArr[0] +' minutes. ' + " The last bus is in " +   timesArr[timesArr.length-1] + " minutes."
+    
+}
+var bus10Schedule = [12, 24, 35, 47, 60];
+nextAndLastBusTimes(bus10Schedule);
 
