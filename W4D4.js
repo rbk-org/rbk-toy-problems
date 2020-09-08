2f8511fecee6cfde3b4f7f6a3c93d188f4f23e07
RamiNawahda <rami.nawahda@gmail.com>
2020-09-08 09:41:32 +0200

    W4D4 Rami Toy Problem

--- W4D4.js
+++ W4D4.js
@@ -14,7 +14,13 @@ Calling your function should result in something like:
 myFavoriteSong(); //{title: "Countdown", artist: "Beyonce", durationInSeconds: 212}
 
 */
-
+function myFavoriteSong() {
+var obj={};
+	obj.title="Countdown";
+	obj.artist = "Beyonce";
+	obj.durationInSeconds = 212;
+	return obj;
+}
 /*
 2-Write a function called nextTwoBusTimes that takes an array of numbers representing (in minutes) 
 when the busses come for the day. 
@@ -28,7 +34,10 @@ Calling your function should result in something like:
 var bus10Schedule = [12, 24, 35, 47, 60];
 nextTwoBusTimes(bus10Schedule); //"The next 2 busses arrive in 12 and 24 minutes."
 */
-
+var bus10Schedule = [12, 24, 35, 47, 60];
+function nextTwoBusTimes(timesArr) {
+	return "The next 2 busses arrive in " + timesArr[0] + " and " + timesArr[1] +" minutes.";
+}
 
 /*
 
@@ -44,5 +53,9 @@ Calling your function should result in something like:
 var bus10Schedule = [12, 24, 35, 47, 60];
 nextAndLastBusTimes(bus10Schedule); //"The next bus is in 12 minutes. The last bus is in 60 minutes.
 */
-
+var bus10Schedule = [12, 24, 35, 47, 60];
+function nextAndLastBusTimes(timesArr) {
+	var l = timesArr.length-1;
+	return "The next bus is in " + timesArr[0] + ". The last bus is in  " + timesArr[l] +" minutes." 
+}
 
