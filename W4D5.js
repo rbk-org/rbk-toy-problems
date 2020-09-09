1be10e97619f55487622ffd2be3e77c7e7a86a0b
RamiOkkeh <ramiokkeh@gmail.com>
2020-09-09 09:48:20 +0300

    W4D5

--- W4D5.js
+++ W4D5.js
@@ -3,7 +3,7 @@
 and returns a new array of people and their birthday information who were all born in the given month.
 
 A sample input array would look like:
-
+*/
 var staff = [
   {name: {first: "Alyssa", last: "Hacker"}, birthDay: {month: "June", day: 5, year: 1987}},
   {name: {first: "Ben", last: "Bitdiddle"}, birthDay: {month: "August", day: 19, year: 1984}},
@@ -11,6 +11,7 @@ var staff = [
   {name: {first: "Lem", last: "Tweakit"}, birthDay: {month: "August", day: 11, year: 1989}},
   {name: {first: "Louis", last: "Reasoner"}, birthDay: {month: "November", day: 17, year: 1992}}
 ];
+/*
 A call to your function getPeopleBornIn(staff, 'August');
 
 Would return:
@@ -18,4 +19,13 @@ Would return:
 ["Ben Bitdiddle: August 19, 1984", "Lem Tweakit: August 11, 1989"]
 
 */
-
+function getPeopleBornIn(arr, month) {
+	var res = []
+	for (var i = 0; i < arr.length; i++) {
+		if (month === arr[i]['birthDay']['month']) {
+			res.push(arr[i]['name']['first'] + ' ' + arr[i]['name']['last'] + ': ' + arr[i]['birthDay']['month'] + ' ' + arr[i]['birthDay']['day'] + ', ' + arr[i]['birthDay']['year'])
+		}
+	}
+	return res
+}
+console.log(getPeopleBornIn(staff, 'August'))
