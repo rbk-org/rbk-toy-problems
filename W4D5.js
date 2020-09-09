2c2f14ca275a7734dd4983b86b7116cec0df46a8
Rasha <r.alamleh@gmail.com>
2020-09-09 10:29:33 +0300

    W4D5

--- W4D5.js
+++ W4D5.js
@@ -18,4 +18,23 @@ Would return:
 ["Ben Bitdiddle: August 19, 1984", "Lem Tweakit: August 11, 1989"]
 
 */
+var staff = [
+  {name: {first: "Alyssa", last: "Hacker"}, birthDay: {month: "June", day: 5, year: 1987}},
+  {name: {first: "Ben", last: "Bitdiddle"}, birthDay: {month: "August", day: 19, year: 1984}},
+  {name: {first: "Eva", last: "Ator"}, birthDay: {month: "March", day: 29, year: 1980}},
+  {name: {first: "Lem", last: "Tweakit"}, birthDay: {month: "August", day: 11, year: 1989}},
+  {name: {first: "Louis", last: "Reasoner"}, birthDay: {month: "November", day: 17, year: 1992}}
+];
+
+function getPeopleBornIn(array, string) {
+  return map(array, function(obj,i){
+    if (obj.birthDay.month === string) {
+      obj.name = 'obj.name.first '+ 'obj.name.last ';
+      obj.birthDay = 'obj.birthDay.month '+ 'obj.birthDay.day '
+    } 
+  })
+	}
+
+
+
 
