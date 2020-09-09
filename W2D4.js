f2a2f853987d0acf3ed1f484b4bf49f3f81543fc
RamiOkkeh <ramiokkeh@gmail.com>
2020-09-09 21:49:53 +0300

    W4D5

--- W2D4.js
+++ W2D4.js
@@ -1,8 +1,22 @@
 //1.Write a function called sameLength that takes two strings as parmeters, 
 //and returns true if those strings have the same length, and false otherwise.
+function sameLength(str1,str2) {
+	//compare the two strings
+	if (str1.length === str2.length) {
+		//return true if they are equal
+		return true;
+	}
+	//return false if they are not 
+	return false;
+}
+console.log(sameLength('rami','okkeh'))
+
 
 //2.Write a function called passwordLongEnough that takes a 'password' as a parameter 
 //and returns true if that password is long enough -- you get to decide what constitutes long enough.
+ function passwordLongEnough(pass) {
+ 	//check passworld length
+ }
 
 //3.Write a function called rentalCar that takes a person's name and age as parmeters, 
 //and returns either 'You cannot have the keys, .', or "Have fun driving", 
