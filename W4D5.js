adaa70a797f5c13c9b526029d435431ae8d4d56c
HanadiSabarnah <hsabarnah@gmail.com>
2020-09-09 10:32:50 +0300

    W3D5

--- W4D5.js
+++ W4D5.js
@@ -19,3 +19,22 @@ Would return:
 
 */
 
+function getPeopleBornIn(array,string){
+var arr =[]
+for(var i = 0 ; i < array.length ; i++){
+	if(array[i].birthday.month === 'string'){
+		return array[i].name + " Bitdiddle: " + 
+		
+	}
+}
+
+}
+var staff = [
+  {name: {first: "Alyssa", last: "Hacker"}, birthDay: {month: "June", day: 5, year: 1987}},
+  {name: {first: "Ben", last: "Bitdiddle"}, birthDay: {month: "August", day: 19, year: 1984}},
+  {name: {first: "Eva", last: "Ator"}, birthDay: {month: "March", day: 29, year: 1980}},
+  {name: {first: "Lem", last: "Tweakit"}, birthDay: {month: "August", day: 11, year: 1989}},
+  {name: {first: "Louis", last: "Reasoner"}, birthDay: {month: "November", day: 17, year: 1992}}
+];
+
+
