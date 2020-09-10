62e46c6a2144cc97d94a861b85bd310d460f41ce
Samar-Dawoud <70088338+Samar-Dawoud@users.noreply.github.com>
2020-09-10 09:59:48 +0300

    W4D6.js

--- W4D6.js
+++ W4D6.js
@@ -4,3 +4,21 @@
 //Create a function that takes a student as parameter and print out the details of the student
 //Create a function that takes the array of students and sort them out based on age (younger-to-older)
 //Create a function that takes the array and a string either “M” or “F” and returns the male students if “M” was passed and female students if “F” was passed
+
+
+function Student(){
+	var obj={};
+	obj.name="Samar"
+	obj.date="14/7/1999"
+	obj.gender="female"
+	obj.age=21
+	obj.student=["st1","st2","st3","st4","st5"]
+	obj.st=st
+	return obj
+}
+var student1= Student();
+function st(student){
+	return this.name+"  "+ this.date+"  "+this.gender +"  "+this.age
+}
+console.log(student1);
+//i can't remeber oop very well i will try solve it again 
