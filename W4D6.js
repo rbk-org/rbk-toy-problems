acc437c70658a61f1c69426a0ef6d3478912b305
kholoudmohtaseb <kholoud.almohtseb@gmail.com>
2020-09-10 13:14:46 +0300

    W4D5

--- W4D6.js
+++ W4D6.js
@@ -4,3 +4,49 @@
 //Create a function that takes a student as parameter and print out the details of the student
 //Create a function that takes the array of students and sort them out based on age (younger-to-older)
 //Create a function that takes the array and a string either “M” or “F” and returns the male students if “M” was passed and female students if “F” was passed
+function Student(Name){
+	var obj = {};
+	obj.Name = Name;
+	obj.DateOfBirth;
+	obj.Gender;
+	obj.Age;
+	obj.details = details;
+	obj.SortAsAge = SortAsAge;
+	obj.passed = true;
+	obj.CheckPassed = passed;
+var students = [ {Name:'Ahmad', DateofBirth:2000, Gender:'male' , Age:20 },
+              {Name:'Rana', DateofBirth:2000, Gender:'female' , Age:20 },
+              {Name:'Rami', DateofBirth:2002, Gender:'male' , Age:18 },
+              {Name:'Kareem', DateofBirth:2001, Gender:'male' , Age:19 },
+              {Name:'Lana', DateofBirth:2001, Gender:'female' , Age:19 } ]
+	return obj;
+}
+//---------------------------------
+function details(students){
+this.DateOfBirth = DateOfBirth;
+this.Gender = Gender;
+this.Age = Age;
+
+return students;	
+}
+//----------------------------------
+function SortAsAge(students){
+	sort = [];
+	for (var i = 0; i < staff.length; i++){
+		if (students[i]['age'] >= students[i]['age']){
+			sort.push(students[i]);
+            }
+		}
+		return sort;
+
+
+//-------------------------------------------------
+function passed(){
+	return this.passed;
+}
+
+function CheckPassed(str){
+	if (this.passed === 'M' || this.passed === 'F'){
+		return true;
+       }
+      }
