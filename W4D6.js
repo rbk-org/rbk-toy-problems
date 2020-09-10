8f4328e3ccfc480fad1de7def34d1d27074c1ae2
shaima96 <141062@ppu.edu.ps>
2020-09-10 09:57:24 +0300

    W4D6

--- W4D6.js
+++ W4D6.js
@@ -1,6 +1,55 @@
-//Create a function (class) called Student that return an object that contains the following properties:
-//Name, Date of Birth, Gender, Age
-//Create an array and fill it with 5 students
-//Create a function that takes a student as parameter and print out the details of the student
-//Create a function that takes the array of students and sort them out based on age (younger-to-older)
-//Create a function that takes the array and a string either “M” or “F” and returns the male students if “M” was passed and female students if “F” was passed
+
+
+
+function Student(name,dateOfBirth,gender,age)
+{
+  var obj={}
+  obj.name=name;
+  obj.dateOfBirth=dateOfBirth;
+  obj.gender=gender;
+  obj.age=age;
+  
+  obj.studentInfo=studentInfo;
+  obj.sortStudent=sortStudent;
+  obj.checkGender=checkGender;
+  return obj;
+}
+
+
+var studentArray=[
+{name:"Ahmed",dateOfBirth:"12/6/2000",gender:"Male",age:20},
+{name:"A",dateOfBirth:"12/6/1996",gender:"Male",age:24},
+{name:"b",dateOfBirth:"12/6/2000",gender:"Female",age:20},
+{name:"c",dateOfBirth:"12/6/2020",gender:"Male",age:1},
+{name:"d",dateOfBirth:"12/6/1994",gender:"Female",age:26}
+];
+
+function studentInfo(array){
+  return "name: "+this.name+" dateOfBirth: "+this.dateOfBirth+"  gender: "+this.gender+"  age: "+this.age;
+
+console.log("info",studentInfo(studentArray));
+
+
+ function sortStudent(studentsArray){
+	var arr1=[];  arr1[0]=studentsArray[0]
+	for (var i=0; i<studentsArray.length; i++){
+		if (arr1[0].age > studentsArray[i].age ){
+			arr1[i]=studentsArray[i];
+		}
+	}
+	return array1;
+}
+
+sortStudent(studentArray)
+
+
+
+function checkGender(array){
+for(var i=0;i<array.length;i++){ 
+if(array[i].gender[0] === "F"){
+  return "Female";
+}
+return "Male";
+}
+}
+console.log("check",checkGender(studentArray));
