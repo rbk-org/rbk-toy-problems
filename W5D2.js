1404a09cdf23674784f9d9b2679f0b46d2135b35
ameedasmah <ameed.asmah@hotmail.com>
2020-09-13 09:23:47 +0300

    W5D2

--- W5D2.js
+++ W5D2.js
@@ -2,28 +2,47 @@
 1) implement the last version of each 
 */
 
-function each(coll, func) {
- //write your code here  
+function each(coll, f) {
+  if (Array.isArray(coll)) {
+    for (var i = 0; i < coll.length; i++) {
+      f(coll[i], i);
+    }
+  } else {
+    for (var key in coll) {
+      f(coll[key], key);
+    }
+  }
 }
-	
+
 
 /*
 1) implement map function
 
 */
-	function map(array, f) {
-	  var acc = [];
-	  
-	  return acc;
-	}
+	function map(array, func) {
+  var acc = [];
+  each(array, function(element) {
+    acc.push(func(element));
+  });
+  return acc;
+}
 
 /*
 1) implement the improved map 
 */
 
 
-function map(coll, f) {
-	var acc = [];
-	  
-	 return acc;
-}
+function everymap(coll,func){
+    var acc = []
+  if(Array.isArray(coll)){
+    each(coll,function(value,i){
+      acc.push(func(value,i))
+    })
+  }
+  else {
+       acc ={}
+    each(coll,function(value,key){
+      acc[key]=func(value,key)
+    })
+    }
+  return acc
