963132bf2a7ba38dd93518257658c5fa043ecda5
asemnouri <64498291+asemnouri@users.noreply.github.com>
2020-09-13 09:11:36 +0300

    W5D2

--- W5D2.js
+++ W5D2.js
@@ -4,6 +4,11 @@
 
 function each(coll, func) {
  //write your code here  
+ if(Array.isArray(coll)){
+    for(i=0;i<coll.length;i++){
+      func(coll[i],i)}}
+    else{for(k in coll){
+      func(coll[k],k)}}
 }
 	
 
@@ -13,8 +18,13 @@ function each(coll, func) {
 */
 	function map(array, f) {
 	  var acc = [];
-	  
-	  return acc;
+		if (!Array.isArray(coll)) {
+		  acc = {}
+		}
+		each(array, function(element, i) {
+		  acc[i] = f(element, i)
+		})
+		return acc
 	}
 
 /*
@@ -24,6 +34,11 @@ function each(coll, func) {
 
 function map(coll, f) {
 	var acc = [];
-	  
+		if (!Array.isArray(coll)) {
+		  acc = {}
+		}
+		each(coll, function(element, key) {
+		  acc[key] = f(element, key)
+		})
 	 return acc;
 }
