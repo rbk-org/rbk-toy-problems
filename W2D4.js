3b4327afc0708dbe503e67bfade3054120223adb
RamiOkkeh <ramiokkeh@gmail.com>
2020-09-13 19:28:24 +0300

    W5D2

--- W2D4.js
+++ W2D4.js
@@ -1,30 +1,46 @@
 //1.Write a function called sameLength that takes two strings as parmeters, 
 //and returns true if those strings have the same length, and false otherwise.
-function sameLength(str1,str2) {
-	//compare the two strings
-	if (str1.length === str2.length) {
-		//return true if they are equal
-		return true;
-	}
-	//return false if they are not 
-	return false;
+function sameLength(str1, str2) {
+	return (str1.length === str2.length)
 }
-console.log(sameLength('rami','okkeh'))
-
-
+console.log(sameLength('rami Okkeh', 'helloworld')) // result = true
 //2.Write a function called passwordLongEnough that takes a 'password' as a parameter 
 //and returns true if that password is long enough -- you get to decide what constitutes long enough.
- function passwordLongEnough(pass) {
- 	//check passworld length
- }
-
+function passwordLongEnough(pass) {
+	return (pass.length > 5) 
+}
+console.log(passwordLongEnough('12345')) // result = false
+console.log(passwordLongEnough('567890')) // result = true
 //3.Write a function called rentalCar that takes a person's name and age as parmeters, 
-//and returns either 'You cannot have the keys, .', or "Have fun driving", 
+//and returns either 'You cannot have the keys, {name} .', or "Have fun driving", 
 // depending on whether or not the person is old enough. 
 //In the US, most rental car companies do not allow you to rent a car until you are 21.
-
+function rentalCar(name, age) {
+	if (age > 20) {
+		return 'Have fun driving'
+	} return 'You cannot have the keys, '+name+'.'
+}
+console.log(rentalCar('rami', 16)) // 'You cannot have the keys, rami.'
+console.log(rentalCar('rami', 21))
 // 4.Write a function called max that takes two numbers as parameters, 
 //and returns the larger one.
-
+function max(num1, num2) {
+	var max = num1
+	if (num2 > num1) {
+		max = num2
+	}
+	return max
+}
+console.log(max(20, 30)) // result = 30
+console.log(max(40, 10)) // result = 40
 // 5.Write a function called  min that takes two numbers as parameters,
 // and returns the smaller one.
+function min(num1, num2) {
+	var min = num1
+	if (num2 < num1) {
+		min = num2
+	}
+	return min
+}
+console.log(min(50, 100)) // result = 50
+console.log(min(2, 6)) // result = 2
--- W4D2.js
+++ W4D2.js
@@ -3,15 +3,71 @@
 
 // create a data model to represent your classmates
 // 	-think of different attributes of your classmates? what do all of them have ?
-// 	-create a factory function.
-// 	-create an array to hold the classmates that you created and what you created to it .  
-// 	-write a function called displayFriend that takes a mate as an argument and returns the important information in a readable way.
-// 	-write a function called addFriend that takes a mate as an argument and add it to you classMates arraya.
-// 	-calculate the number of male friends that your class have by writing a function called nbOfMale.
+// var hamdalla = {
+// 	name: 'hamdalla',
+// 	age: 25,
+// 	residense: 'nablus',
+// }
 
+// 	-create a factory function.
+var MakeClassmate = function(name, age, residense, gender) {
+	var obj = {}
+	obj.name = name
+	obj.age = age
+	obj.residense = residense
+	obj.displayFriend = displayFriend
+	obj.gender = gender
+	return obj
+}
+var hamdalla = MakeClassmate('hamdalla', 25, 'nablus', 'male')
+// 	-create an array to hold the classmates that you created and what you created to it .  
+var myClass = [MakeClassmate('rami', 21, 'nablus', 'male'),hamdalla]
+console.log(myClass)
+// 	-write a function called displayFriend that takes a mate as an argument and returns the important information in a readable way.
+function displayFriend() {
+	// console.log('hello')
+	return this.name + ' is ' + this.age + ' years old and lives in ' + this.residense
+}
+console.log(myClass[1].displayFriend())
+// 	-write a function called addFriend that takes a mate as an argument and add it to you classMates array.
+function addFriend(mate) {
+	myClass.push(mate)
+}
+var sufyan = MakeClassmate('sufyan', 30, 'nablus', 'male')
+addFriend(sufyan)
+console.log(myClass)
+// 	-calculate the number of male friends that your class have by writing a function called nbOfMale.
+function numberOfMales(arr) {
+	var count = 0
+	for (var i = 0; i < arr.length; i++) {
+		if (arr[i].gender === 'male') {
+			count++
+		}
+	}
+	return 'there are ' + count + ' males in my class'
+}
+console.log(numberOfMales(myClass))
 
 //Part2:
 // Using recursion Write a JavaScript function to find the greatest common divisor (gcd) of two positive numbers.
+function comDiv(num1, num2, num = num1) {
+	if (num1 % num === 0 && num2 % num === 0) {
+		return num
+	}
+	return comDiv(num1, num2, --num)
+}
+console.log(comDiv(120, 50)) // 10
+console.log(comDiv(150, 50)) // 25
+console.log('\n')
+
 // Write a function called sum that accepts two numbers as parameters, and sum them together but without suming them togther directly
 //you can only add one at each summation, you'll need to use recursion in this.
-
+function sumRec(num1, num2) {
+	if (num2 <= 0) {
+		return num1
+	}
+	return 1 + sumRec(num1, --num2)
+}
+console.log(sumRec(4, 6)) // 10
+console.log(sumRec(9, 3)) // 12
+console.log(sumRec(8, 0)) // 8
