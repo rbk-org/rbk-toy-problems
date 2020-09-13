c0430533422020a2307861ddd3722cd6eefb1088
RamiNawahda <rami.nawahda@gmail.com>
2020-09-13 09:36:14 +0200

    W5D2 toy-1

--- W5D2.js
+++ W5D2.js
@@ -3,19 +3,30 @@
 */
 
 function each(coll, func) {
- //write your code here  
+ //write your code here 
+ if(Array.isArray(coll)){
+ 	for(var i = 0 ; i < coll.length ; i++){
+ 		func(coll[i],i)
+ 	}
+ }else{
+ 	for(var key in coll){
+ 		func(coll[key],key)
+ 	}
+ }
 }
-	
+
 
 /*
 1) implement map function
 
 */
-	function map(array, f) {
-	  var acc = [];
-	  
-	  return acc;
-	}
+function map(array, f) {
+  var acc = [];
+  each(array, function(element,key)){
+  	acc.psuh(f(element,key);
+  }
+  return acc;
+}
 
 /*
 1) implement the improved map 
@@ -24,6 +35,11 @@ function each(coll, func) {
 
 function map(coll, f) {
 	var acc = [];
-	  
-	 return acc;
+	if(!Array.isArray(array)){
+		arr={}
+	}
+	each(array, function(element,key)){
+		acc[key]=f(element,key);
+	}
+	return acc;
 }
