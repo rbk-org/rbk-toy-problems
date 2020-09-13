87707128971f388ab2f5d34e7022d422c9fcb963
Halasalhab <halasalhab0@gmail.com>
2020-09-13 09:26:53 +0300

    Done

--- W5D2.js
+++ W5D2.js
@@ -1,9 +1,17 @@
 /*
 1) implement the last version of each 
 */
+function each(array,f){
+  if(!Array.isArray(array)){
+    for(var i=0;i<array.length;i++){
+      f(array[i],i)
+    }
+  } else {
+    for(var key in array){
+      f(array[key],key)
+    }
+  }
 
-function each(coll, func) {
- //write your code here  
 }
 	
 
@@ -13,9 +21,13 @@ function each(coll, func) {
 */
 	function map(array, f) {
 	  var acc = [];
+      each(array,function(element,i){
+      acc.push(f(element,i))
+      });
+      return acc
+      }
 	  
-	  return acc;
-	}
+	
 
 /*
 1) implement the improved map 
@@ -24,6 +36,16 @@ function each(coll, func) {
 
 function map(coll, f) {
 	var acc = [];
-	  
-	 return acc;
-}
+	if(!Array.isArray(coll)){
+	acc={}
+	for(var key in coll){
+	acc[key]=f(coll[key])
+
+	}}
+	else{
+	for(var p=0;p<coll.length;p++){
+	acc.push(f(coll[i]))
+	}
+	}
+	return acc;
+    }
