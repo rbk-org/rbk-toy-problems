6538cdf0795735a0f8cb84c79be966d43c87e42c
Qamar19 <48962147+Qamar19@users.noreply.github.com>
2020-09-13 10:44:35 +0300

    W5D2

--- W5D2.js
+++ W5D2.js
@@ -3,27 +3,43 @@
 */
 
 function each(coll, func) {
- //write your code here  
+	if (Array.isArray(coll)) {
+		for (var i = 0; i < coll.length; i++) {
+			f(coll[i], i);
+		}
+	} else {
+		for (var key in coll) {
+			func(coll[key], key);
+		}
+	}
 }
-	
+
+
 
 /*
 1) implement map function
 
 */
-	function map(array, f) {
-	  var acc = [];
-	  
-	  return acc;
-	}
+function map(array, f) {
+	var acc = [];
+	each(array, function (element, i) {
+		acc.push(f(element, i));
+	});
+	return acc;
+}
 
 /*
 1) implement the improved map 
 */
-
-
-function map(coll, f) {
+function map(coll, fun) {
 	var acc = [];
-	  
-	 return acc;
+	if (!Array.isArray(coll)) {
+		acc = {}
+	}
+	each(coll, function (value, key) {
+		acc[key] = fun(value, key)
+	})
+	return acc
+
 }
+//submit
