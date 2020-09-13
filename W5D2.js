8c6bfe91d6261942b7ff357a0e1ec4ed70282abd
aya abuyousef <aya.abuyousef@gmail.com>
2020-09-13 11:42:45 +0300

    w5d2

--- W5D2.js
+++ W5D2.js
@@ -1,9 +1,18 @@
 /*
-1) implement the last version of each 
+1) 
+
 */
 
 function each(coll, func) {
- //write your code here  
+ if (Array.isArray(coll)){
+ 	for (var i=0;i<coll.length;i++){
+ 		func(coll[i],i);
+
+ 	}else{
+ 		for (var key in coll){
+ 			func(coll[key],key);
+ 		}
+ }
 }
 	
 
@@ -13,7 +22,13 @@ function each(coll, func) {
 */
 	function map(array, f) {
 	  var acc = [];
+	  each(array,function(element,key){
+	  	acc.push(f(element,key))
+
 	  
+	  
+	  })
+
 	  return acc;
 	}
 
@@ -24,6 +39,12 @@ function each(coll, func) {
 
 function map(coll, f) {
 	var acc = [];
-	  
+	  if(!Array.isArray(coll)){
+	  	acc={}
+
+	  }
+	  each(coll,function(element,key){
+	  	acc[key]=f(element,key)
+	  })
 	 return acc;
 }
