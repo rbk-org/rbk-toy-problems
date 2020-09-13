4af89048d31f0df46670af613aea6126a3c0c7e7
Samar-Dawoud <70088338+Samar-Dawoud@users.noreply.github.com>
2020-09-13 09:38:16 +0300

    W5D2

--- W5D2.js
+++ W5D2.js
@@ -3,7 +3,18 @@
 */
 
 function each(coll, func) {
- //write your code here  
+ //write your code here 
+ if(Array.isArray(coll)){
+ for(var i=0;i<coll.length;i++){
+   func(coll[i],i)
+ }
+ }
+ else{
+ for(var key in coll){
+ func(coll[key],key)
+ }
+ } 
+
 }
 	
 
@@ -13,9 +24,12 @@ function each(coll, func) {
 */
 	function map(array, f) {
 	  var acc = [];
-	  
+	  each(array,function(element,i){
+      acc.push(f(element,i));
+    })
 	  return acc;
 	}
+
 
 /*
 1) implement the improved map 
@@ -24,6 +38,11 @@ function each(coll, func) {
 
 function map(coll, f) {
 	var acc = [];
-	  
+	  if(!Array.isArray(coll)){
+	  	acc={}
+	  }
+	   each(coll,function(element,key){
+      acc[i]=(f(element,key));
+    })
 	 return acc;
 }
