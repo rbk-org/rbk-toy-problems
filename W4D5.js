f5c6ca03c2ad1631a28c23a700541f19c39f3e7b
HanadiSabarnah <hsabarnah@gmail.com>
2020-09-14 10:05:56 +0300

    update

--- W4D5.js
+++ W4D5.js
@@ -19,3 +19,21 @@ Would return:
 
 */
 
+function getPeopleBornIn(arr, month) {
+	var res = []
+	for (var i = 0; i < arr.length; i++) {
+		if (month === arr[i]['birthDay']['month']) {
+			res.push(arr[i]['name']['first'] + ' ' + arr[i]['name']['last'] + ': ' + arr[i]['birthDay']['month'] + ' ' + arr[i]['birthDay']['day'] + ', ' + arr[i]['birthDay']['year'])
+		}
+	}
+	return res
+}
+
+var staff = [
+  {name: {first: "Alyssa", last: "Hacker"}, birthDay: {month: "June", day: 5, year: 1987}},
+  {name: {first: "Ben", last: "Bitdiddle"}, birthDay: {month: "August", day: 19, year: 1984}},
+  {name: {first: "Eva", last: "Ator"}, birthDay: {month: "March", day: 29, year: 1980}},
+  {name: {first: "Lem", last: "Tweakit"}, birthDay: {month: "August", day: 11, year: 1989}},
+  {name: {first: "Louis", last: "Reasoner"}, birthDay: {month: "November", day: 17, year: 1992}}
+];
+getPeopleBornIn(staff, 'August')
--- W4D6.js
+++ W4D6.js
@@ -4,3 +4,20 @@
 //Create a function that takes a student as parameter and print out the details of the student
 //Create a function that takes the array of students and sort them out based on age (younger-to-older)
 //Create a function that takes the array and a string either “M” or “F” and returns the male students if “M” was passed and female students if “F” was passed
+function Student(name, dateOfBirth, gender, age) {
+	var obj = {}
+	obj.name = name
+	obj.dateOfBirth = dateOfBirth
+	obj.gender = gender
+	obj.age = age
+	return obj
+}
+var myClass = [Student('rami', 2000, 'male', 21), Student('ahmad', 1994, 'male', 26), Student('hiba', 1995, 'female', 25), Student('ali', 1996, 'male', 24), Student('hanadi', 1987, 'female', 32)]
+
+console.log(myClass)
+
+function displayStudent(student) {
+	return student.name + ' born in ' + student.dateOfBirth + ' and is a ' + student.age + ' year old ' + student.gender
+}
+
+displayStudent(myClass)
