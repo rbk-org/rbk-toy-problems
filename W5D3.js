6519e80f3633aba8e2d042e5242fe1d85e99f45f
israakittaneh <israakittanehm@gmail.com>
2020-09-14 09:42:05 +0300

    W5D3

--- W5D3.js
+++ W5D3.js
@@ -2,8 +2,28 @@
 1- Using map, Write a function called uppercaseValues that, given an object as a parameter,
  returns a new object with all of its string values converted to uppercase. 
 Also, ensure that you only attempt to convert strings to uppercase.
-*/
 
+*/
+function each(array , func){
+	for(var i = 0 ; i < array.length ; i++){
+		func(array[i] , i)
+	}
+}
+function map(coll , f){
+	var acc = [];
+	if(!Array.isArray(coll)){
+     acc = {};
+	}each(coll , function(element , key){
+		acc[key]=f(element , key);
+	})
+	return acc;
+}
+function uppercaseValues(object){
+	return map(object , function(value , key){
+         if(typeof(value) === "string")
+		return value.toUpperCase();
+	} )
+}
 
 
 
@@ -14,3 +34,4 @@ function sum(numbers) {
 	//your code is here 
 
 }
+//i will solve it afetr studided these matrial ,,because i wasn't these day
