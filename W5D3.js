95392f906d99e133740a6e31169b6418505a9527
asemnouri <64498291+asemnouri@users.noreply.github.com>
2020-09-14 09:38:20 +0300

    W5D3

--- W5D3.js
+++ W5D3.js
@@ -3,14 +3,47 @@
  returns a new object with all of its string values converted to uppercase. 
 Also, ensure that you only attempt to convert strings to uppercase.
 */
-
+function each(coll, func) {
+	//write your code here  
+	if(Array.isArray(coll)){
+	   for(i=0;i<coll.length;i++){
+		 func(coll[i],i)}}
+	   else{for(k in coll){
+		 func(coll[k],k)}}
+   }
+function map(coll, f) {
+	var acc = [];
+		if (!Array.isArray(coll)) {
+		  acc = {}
+		}
+		each(coll, function(element, key) {
+		  acc[key] = f(element, key)
+		})
+	 return acc;
+}
+function uppercaseValues(object){
+	return map(object,function(element,key){
+		return element.toUpperCase()
+	})
+}
+var obj={a:'asem',b:'ameed'}
 
 
 
 /*
 2- Using reduce, write a function that sums the squared values of an array of numbers and returns the sum result.
 */
+function reduce(array,f,start){
+	var acc=start
+	each(array,function(element,key){
+	  acc=f(acc,element,key)
+	})
+	return acc
+	}
 function sum(numbers) {
 	//your code is here 
+	return reduce(numbers,function(acc,number){
+		return acc+number
+	},0)
 
 }
