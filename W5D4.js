696cb38d4cccf9045ad045dcaf07a45b7900d21b
israakittaneh <israakittanehm@gmail.com>
2020-09-15 09:33:15 +0300

    W5D4

--- W5D4.js
+++ W5D4.js
@@ -14,14 +14,32 @@ function each(coll, f) {
 1-write the the filter function
 */
 
-function filter() {
+function filter(array , predicate) {
   // your code is here
+  var acc=[];
+  each(array , function(element , i){
+    if(predicate(element)){
+    acc.push(element);
+  }
+  } );
+ 
+ return acc;
+
 }
 
 /*
 2-write the the new version of reduce function
 */
 
-function reduce() {
+function reduce(arr , func , acc) {
   // your code is here
+  if(acc === undefined){
+    acc = arr[0];
+    arr = arr.slice(1);
+  }
+  each(arr , function(element , i){
+    acc = func(acc , element ,i);
+  });
+  return acc;
+
 }
