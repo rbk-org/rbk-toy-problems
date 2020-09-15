33f0bf2d0d1912ac04d318147a6748af1f295a79
NajwanQandeel <najwan.qandeel@gmail.com>
2020-09-15 09:25:32 +0300

    W5D4

--- W5D4.js
+++ W5D4.js
@@ -14,14 +14,34 @@ function each(coll, f) {
 1-write the the filter function
 */
 
-function filter() {
-  // your code is here
+function filter(coll, predicate) {
+  var acc = {};
+  if (Array.isArray(coll)) {
+    acc = [];
+  }
+  each(coll, function (element, key) {
+    if (predicate(element)) {
+      if (Array.isArray(acc)) {
+        acc.push(element);
+      }
+      acc[key] = element;
+    }
+  });
+  return acc;
 }
+
 
 /*
 2-write the the new version of reduce function
 */
 
-function reduce() {
-  // your code is here
+function reduce(array, f, acc) {
+  if (acc === undefined) {
+    acc = array[0];
+    array = array.slice(1);
+  }
+  each(array, function (element, i) {
+    acc = f(acc, element, i);
+  });
+  return acc;
 }
