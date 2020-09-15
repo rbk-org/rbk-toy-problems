36fd1fee7b7d761e33a4eca547cb6e7970c6018f
Samar-Dawoud <70088338+Samar-Dawoud@users.noreply.github.com>
2020-09-15 09:46:20 +0300

    W5D4

--- W5D4.js
+++ W5D4.js
@@ -1,27 +1,31 @@
-function each(coll, f) {
-  if (Array.isArray(coll)) {
-    for (var i = 0; i < coll.length; i++) {
-      f(coll[i], i);
-    }
-  } else {
-    for (var key in coll) {
-      f(coll[key], key);
-    }
-  }
-}
-
 /*
 1-write the the filter function
 */
 
-function filter() {
+function filter(array,predicate) {
   // your code is here
+  var acc=[];
+each(array,function(element){
+  if(predicate(element)){
+    acc.push(element)
+  }
+})
+  return acc;
 }
 
 /*
 2-write the the new version of reduce function
 */
 
-function reduce() {
+function reduce(arr,f,acc) {
   // your code is here
+  if(acc === undefined){
+    acc=arr[0];
+    arr.splice(1);
+  }
+  each(arr,function(element,i){
+    acc=f(element,i)
+  })
+  return acc;
 }
+
