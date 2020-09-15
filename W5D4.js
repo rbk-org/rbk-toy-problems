ad17ec1f3290be7a3aa970bee32161c07d7e5b75
Rasha <r.alamleh@gmail.com>
2020-09-15 09:31:10 +0300

    Done

--- W5D4.js
+++ W5D4.js
@@ -14,14 +14,27 @@ function each(coll, f) {
 1-write the the filter function
 */
 
-function filter() {
-  // your code is here
+function filter(coll , predicate) {
+	var acc =[];
+	each(coll, function(ele){
+		if(predicate(ele)){
+			acc.push(ele)
+		}
+	})
+	return acc;
 }
 
 /*
 2-write the the new version of reduce function
 */
 
-function reduce() {
-  // your code is here
+function reduce(array , fn ,acc) {
+	if(acc === undefined) {
+		acc = array[0];
+		array = array.slice(1)
+	}
+	each (array , function(ele,i) {
+		acc = fn(acc , ele , i)
+	});  
+	return acc
 }
