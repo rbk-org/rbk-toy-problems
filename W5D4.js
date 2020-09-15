61270a7f82e61d63250f095ebd598d6076891ef3
RamiNawahda <rami.nawahda@gmail.com>
2020-09-15 09:26:33 +0200

    W5D4 toyProb-1

--- W5D4.js
+++ W5D4.js
@@ -14,14 +14,59 @@ function each(coll, f) {
 1-write the the filter function
 */
 
-function filter() {
+function filter(coll,predicate) {
   // your code is here
+  var acc=[]
+  each(coll, function(element,key){
+  	if(predicate(element,key)){
+  		acc.push(element)
+  	}
+  })
+  return acc;
+}
+
+//New version of filter 
+function filter(coll,predicate){
+  var acc=[];
+  if(!Array.isArray(coll)){
+  	acc={};
+    each(coll,function(element,key){
+      if(predicate(element,key)){
+        acc[key]=element
+      }
+    })
+  }
+  else{
+    each(coll,function(element,key){
+      if(predicate(element,key)){
+      	acc.push(element)
+      }
+    })
+  }
+  return acc
 }
 
 /*
 2-write the the new version of reduce function
 */
 
-function reduce() {
-  // your code is here
+function reduce(coll,f,start){
+	if (!Array.isArray(coll)){
+		var acc = start;
+		if(acc===undefined){
+			index=Object.keys(coll)[0]
+			acc=coll[index];
+			delete coll[index]
+		}
+	}else{
+		var acc = start;
+		if(acc===undefined){
+			acc = coll[0]
+			coll=coll.slice(1);
+		}
+	}
+	each(coll,function(element){
+		acc = f(acc,element);
+	})
+	return acc
 }
