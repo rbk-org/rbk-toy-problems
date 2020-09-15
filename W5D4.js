4e78dd1980b9733296c1c60d27669179859d975d
Yasir-Tbaileh <yasur96@gmail.com>
2020-09-15 09:52:46 +0300

    W5D4 solved

--- W5D4.js
+++ W5D4.js
@@ -14,14 +14,40 @@ function each(coll, f) {
 1-write the the filter function
 */
 
-function filter() {
+function filter(coll, predicate) {
   // your code is here
+  var acc = [];
+  if (!Array.isArray(coll)) {
+    acc = {};
+  }
+  each(coll, function (element, key) {
+    if (predicate(element)) {
+      acc[key] = element;
+    }
+  });
+  return acc;
 }
 
 /*
 2-write the the new version of reduce function
 */
 
-function reduce() {
+function reduce(array, f, acc) {
   // your code is here
+  if (acc === undefined) {
+    acc = array[0];
+    array = array.slice(1);
+
+    each(array, function (element) {
+      acc = f(acc, element);
+    });
+    return acc;
+  }
+  else {
+    var newAcc = acc;
+    each(array, function (element) {
+      newAcc = f(newAcc, element);
+    });
+    return newAcc;
+  }
 }
