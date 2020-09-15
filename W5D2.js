8f7abf49fb7b88ef5fdceebb6be003e32d2b6468
Yasir-Tbaileh <yasur96@gmail.com>
2020-09-15 10:19:26 +0300

    W5D2 solved

--- W5D2.js
+++ W5D2.js
@@ -4,6 +4,15 @@
 
 function each(coll, func) {
  //write your code here  
+	if (Array.isArray(coll)) {
+		for (var i = 0; i < coll.length; i++) {
+			func(coll[i], i);
+		}
+	} else {
+		for (var key in coll) {
+			func(coll[key], key);
+		}
+	}
 }
 	
 
@@ -11,11 +20,13 @@ function each(coll, func) {
 1) implement map function
 
 */
-	function map(array, f) {
-	  var acc = [];
-	  
-	  return acc;
-	}
+function map(array, f) {
+	var acc = [];
+	each(coll, function (element, key) {
+		acc.push(f(element, key));
+	});
+	return acc;
+}
 
 /*
 1) implement the improved map 
@@ -24,6 +35,11 @@ function each(coll, func) {
 
 function map(coll, f) {
 	var acc = [];
-	  
-	 return acc;
+	if (!Array.isArray(coll)) {
+		acc = {};
+	}
+	each(coll, function (element, key) {
+		acc[key] = f(element, key);
+	});
+	return acc;
 }
