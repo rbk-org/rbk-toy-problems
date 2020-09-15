b89e05d7e95c26fff37696fcc6ddf7b33fb498c3
Yasir-Tbaileh <yasur96@gmail.com>
2020-09-15 09:57:00 +0300

    W5D3 solved

--- W5D3.js
+++ W5D3.js
@@ -3,7 +3,14 @@
  returns a new object with all of its string values converted to uppercase. 
 Also, ensure that you only attempt to convert strings to uppercase.
 */
-
+function uppercaseValues(object) {
+	return map(object, function (value, key) {
+		if (typeof value === "string") {
+			return value.toUpperCase()
+		}
+		return value;
+	});
+}
 
 
 
@@ -12,5 +19,50 @@ Also, ensure that you only attempt to convert strings to uppercase.
 */
 function sum(numbers) {
 	//your code is here 
+	return reduce(numbers, function (sum, num) {
+		return sum + num ** 2;
+	}, 0)
+}
+
+//////////////////////////////////////
+function each(coll, func) {
+	if (Array.isArray(coll)) {
+		for (var i = 0; i < coll.length; i++) {
+			func(coll[i], i);
+		}
+	} else {
+		for (var key in coll) {
+			func(coll[key], key);
+		}
+	}
+}
+function map(coll, f) {
+	var acc = [];
+	if (!Array.isArray(coll)) {
+		acc = {};
+	}
+	each(coll, function (element, key) {
+		acc[key] = f(element, key);
+	});
+	return acc;
+}
+
+function reduce(array, f, acc) {
+	if (acc === undefined) {
+		acc = array[0];
+		array = array.slice(1);
+
+		each(array, function (element) {
+			acc = f(acc, element);
+		});
+		return acc;
+	}
+	else {
+		var newAcc = acc;
+		each(array, function (element) {
+			newAcc = f(newAcc, element);
+		});
+		return newAcc;
+	}
 
 }
