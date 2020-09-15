3d89ff86bf70c68fc59c71ee42c96fd61c4c3d86
HanadiSabarnah <hsabarnah@gmail.com>
2020-09-15 09:17:32 +0300

    W5D4

--- W5D4.js
+++ W5D4.js
@@ -14,14 +14,31 @@ function each(coll, f) {
 1-write the the filter function
 */
 
-function filter() {
-  // your code is here
+function filter(array, predicate) {
+    var acc = []
+    each(array, function (element) {
+        if (predicate(element)) {
+            acc.push(element)
+        }
+    })
+    return acc
 }
 
 /*
 2-write the the new version of reduce function
 */
 
-function reduce() {
-  // your code is here
+
+  function reduce(array, f, acc) {
+    if (acc === undefined) {
+        acc = array[0]
+        array = array.slice(1)
+
+    }
+    each(array, function (element) {
+        acc = f(acc, element)
+    })
+    return acc
 }
+
+
