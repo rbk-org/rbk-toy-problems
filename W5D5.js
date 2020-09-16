8285372192b73c8d6e8373661ab43bd7336823f5
Noor-Maher99 <ward1999111@gmail.com>
2020-09-16 10:06:07 +0300

    W5D5

--- W5D5.js
+++ W5D5.js
@@ -4,10 +4,60 @@ Using filter to write the following function:
 and returns an array of only the even numbers in the parameter that are also multiple of 2
 evenMult([2,3,1,4,10,0,44]) ====> [2,10,44]
 */
+function each(coll, f) {
+  if (Array.isArray(coll)) {
+    for (var i = 0; i < coll.length; i++) {
+      f(coll[i], i);
+    }
+  } else {
+    for (var key in coll) {
+      f(coll[key], key);
+    }
+  }
+}
+//-------------------------
+function filter(array,predicate){
+	var acc=[]
+	each(array,function(element){
+		if (predicate(element)){
+acc.push(element)
+		}
+	});
+	return acc
+}
+//--------------------------
 
+function evenMult(nums){
+	return filter(nums,function(num){
+		return num % 2===0;
+	})
+}
 /*
 Using filter, write a function called endsWithChar that takes two parameters: an array of strings, and a character (e.g. "a"), 
 and returns an array of only the strings that end with that character:
 var words=”we just love RBK world"
 endsWithChar(words, “e"); // => [”we”,”love"]
 */
+function each(coll, f) {
+  if (Array.isArray(coll)) {
+    for (var i = 0; i < coll.length; i++) {
+      f(coll[i], i);
+    }
+  } else {
+    for (var key in coll) {
+      f(coll[key], key);
+    }
+  }
+}
+//-------------------------
+function filter(array,predicate){
+	var acc=[]
+	each(array,function(element){
+		if (predicate(element)){
+acc.push(element)
+		}
+	});
+	return acc
+}
+//----------------------------------
+var words=ust love RBK world"
