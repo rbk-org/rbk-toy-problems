902efd2c7c9aa79d36d151885acb18d8112d5eeb
unknown <noorashqar123@gmail.com>
2020-09-19 10:01:23 +0300

    done

--- W6D1.js
+++ W6D1.js
@@ -14,3 +14,39 @@
 // you can use this code for the div content: 
 // Heading for the div
 // I'm a simple html paragraph and the css can controll my attributes. The JS will make the page interactive
+//____________________________________________________HTML CODE _____________________________________________________________
+// <!DOCTYPE html>
+// <html>
+// <head>
+// 	<title></title>
+// 	<link rel="stylesheet" type="text/css" href="style.css">
+// <script src="https://code.jquery.com/jquery-3.5.1.min.js" 
+// integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" 
+// crossorigin="anonymous"></script>
+// </head>
+// <body>
+// 	<div id="input-container">
+// 			<h1 id="main-header">header1</h1>
+// 			<p>rbk toy problem w6d1</p>
+// 	</div>
+// 		<button id="button1">Important</button>
+// 		<button id="button2">normal</button>
+// 		<button id="button3">rest</button>
+// 	<script type="text/javascript" src="script.js"></script>
+
+// </body>
+// </html>
+// _____________________________________________________________JS CODE________________________________________________________
+// $("#main-header").text("Todo App");
+// $("#button1").on("click",function(){
+// 	$("#input-container").css("color","white");
+// 	$("body").css("background-color","red");
+// });
+// $("#button2").on("click",function(){
+// 	$("#input-container").css("color","white");
+// 	$("body").css("background-color","lightblue");
+// });
+// $("#button3").on("click",function(){
+// 	$("#input-container").css("color","black");
+// 	$("body").css("background-color","white");
+// });
