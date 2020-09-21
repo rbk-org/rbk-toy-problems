ea10e36db5db205b2a5c0a03c9cb2d413cc2fc2b
Samar-Dawoud <70088338+Samar-Dawoud@users.noreply.github.com>
2020-09-21 19:35:52 +0300

    W6D1

--- W6D1.js
+++ W6D1.js
@@ -1,4 +1,4 @@
-// 1. create new HTML file a with a three buttons and div with heading and paragraph inside it.
+<!-- // 1. create new HTML file a with a three buttons and div with heading and paragraph inside it.
 //      a. button1 : Important
 //      b. button2 : normal
 //      c. button3 : rest
@@ -13,4 +13,61 @@
 
 // you can use this code for the div content: 
 // Heading for the div
-// I'm a simple html paragraph and the css can controll my attributes. The JS will make the page interactive
+// I'm a simple html paragraph and the css can controll my attributes. The JS will make the page interactive -->
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <script src="https://code.jquery.com/jquery-3.5.1.min.js" 
+        integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="style.css">
+       
+    </head>
+    <body>
+        <button id="button1"> Important</button>
+        <button  id="button2"> normal</button>
+        <button id="button3"> rest</button>
+        <div>
+            <h1> Heading for the div</h1>
+            <p> I'm a simple html paragraph and the css can controll my attributes. The JS will make the page interactive</p>
+        </div> 
+        <script type="text/javascript" src="s.js">
+        </script>
+    </body>
+   
+</html>
+
+/////js
+$('#button1').click(function(){
+    $("p").css("background-color", "red");
+    $("p").css("color", "white");
+    $("h1").css("color", "darkred");
+});
+
+$("#button2").click(function(){
+    $("p").css("background-color", "lightblue");
+    $("p").css("color", "white");
+    $("h1").css("color", "darkblue");
+});
+
+$("#button3").click(function(){
+    $("p").css("background-color", "white");
+    $("p").css("color", "dark");
+   
+});
+/////css
+body{
+    text-align: center;
+    margin: 60px;
+}
+button{
+    border: none;
+    font-size: 16px;
+    font-weight: bold;
+}
+h1{
+    color: palevioletred;
+    font-style: italic;
+    
+    
+}
